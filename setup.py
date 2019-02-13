a748f6ddbbae50065bc3782f0d5ac36ee75dc2aa
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2019-02-13 13:48:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,36 +17,37 @@ with open('README.rst') as readme_file:
 setup(
     name='Auto-FOX',
     version=version['__version__'],
-    description="A tool for parameterizing forcefields by reproducing radial distribution functions.",
+    description='A tool for parameterizing forcefields by reproducing radial distribution functions.',
     long_description=readme + '\n\n',
-    author="Bas van Beek",
+    author='Bas van Beek',
     author_email='b.f.van.beek@vu.nl',
     url='https://github.com//Auto-FOX',
-    packages=[
-        'Auto-FOX',
-    ],
-    package_dir={'Auto-FOX':
-                 'Auto-FOX'},
+    packages=['FOX'],
+    package_dir={'FOX': 'FOX'},
     include_package_data=True,
     license="GNU General Public License v3 or later",
     zip_safe=False,
-    keywords='Auto-FOX',
+    keywords=['quantum-mechanics', 
+              'science', 
+              'chemistry', 
+              'python-3', 
+    ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry'
+        'License :: OSI Approved :: GNU Lesser General Public License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
-    install_requires=[],  # FIXME: add your package's dependencies to this list
+    install_requires=[
+        'numpy', 
+        'scipy', 
+        'pandas'
+    ],
     setup_requires=[
-        # dependency for `python setup.py test`
         'pytest-runner',
-        # dependencies for `python setup.py build_sphinx`
         'sphinx',
         'sphinx_rtd_theme',
         'recommonmark'
@@ -57,6 +58,7 @@ setup(
         'pycodestyle',
     ],
     extras_require={
-        'dev':  ['prospector[with_pyroma]', 'yapf', 'isort'],
+        'test': ['pytest', 'pytest-cov', 'pytest-mock', 'nbsphinx', 'pygraphviz', 'pycodestyle'],
+        'doc': ['sphinx', 'sphinx_rtd_theme', 'nbsphinx']
     }
 )
