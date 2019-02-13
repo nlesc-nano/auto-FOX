43cdb7a0fa8eeb4062e218bf4767c0f1f82349b1
Bas van Beek <b.f.van.beek@vu.nl>
2019-02-13 14:06:46 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -149,7 +149,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Auto-FOX.tex', u'Automated Forcfield Optimization Extension Documentation',
-     u"Bas van Beek", 'manual'),
+     u"B. F. van Beek", 'manual'),
 ]
 
 
