3a57ee0035ae0fc8f01801e93e0add454f37f034
Bas van Beek <b.f.van.beek@vu.nl>
2019-02-13 18:10:50 +0100

    WiP

--- FOX/functions/md.py
+++ FOX/functions/md.py
@@ -8,3 +8,7 @@ from scm.plams.core.settings import Settings
 from scm.plams.core.functions import (init, finish)
 from scm.plams.interfaces.thirdparty.cp2k import Cp2kJob
 
+
+file_name = '/Users/basvanbeek/Documents/GitHub/auto-FOX/FOX/data/md_cp2k.yaml'
+with open(file_name, 'r') as file:
+    s = Settings(yaml.load(file))
