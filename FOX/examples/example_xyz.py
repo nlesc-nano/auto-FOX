d00e4bbcdb1d87bbe935c709a4a3f5d5a7696fd5
Bas van Beek <b.f.van.beek@vu.nl>
2019-02-13 17:52:08 +0100

    Updated path

--- FOX/examples/example_xyz.py
+++ FOX/examples/example_xyz.py
@@ -7,5 +7,5 @@ from os.path import (join, dirname)
 
 def get_example_xyz(path=None, name='Cd68Se55_26COO_MD_trajec.xyz'):
     """ Return the path + name of the example multi-xyz file. """
-    path = path or dirname(__file__)
+    path = path or 'FOX/data'
     return join(path, name)
