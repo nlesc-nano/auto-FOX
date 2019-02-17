8cb470fdd2c05fa361f5f2620940726832e39928
Bas van Beek <b.f.van.beek@vu.nl>
2019-02-17 14:51:45 +0100

    Documentation style changes; 2 new functions

--- FOX/functions/read_xyz.py
+++ FOX/functions/read_xyz.py
@@ -4,6 +4,9 @@ __all__ = ['read_multi_xyz']
 
 import numpy as np
 
+from scm.plams.mol.atom import Atom
+from scm.plams.mol.molecule import Molecule
+
 
 def read_multi_xyz(xyz_file, ret_idx_dict=True):
     """ Reads a (multi) .xyz file and return a *m*n*3* array with the cartesian coordinates of *m*
@@ -12,27 +15,28 @@ def read_multi_xyz(xyz_file, ret_idx_dict=True):
     :parameter str file: The path + filename of a (multi) .xyz file.
     :parameter bool ret_idx_dict: In addition to returning cartesian coordinates, return a
         dictionary with atomic symbols and matching atomic indices (|str|_: |list|_ [|int|_]).
-    :return: A *m*n*3* array (|np.ndarray|_) of cartesian coordinates and, optionally, a dictionary
-        (|dict|_) with atomic symbols as keys and matching atomic indices as
-        values (|str|_: |list|_ [|int|_]).
+    :return: A 3D array of cartesian coordinates and, optionally, a dictionary
+        with atomic symbols as keys and matching atomic indices as alues.
+    :rtype: *m*n*3* |np.ndarray|_ [|np.float64|_] and, optionally, |dict|_
+        (keys: |str|_, values: |list|_ [|int|_]).
     """
+    # Define constants and construct a dictionary: {atomic symbols: [atomic indices]}
     with open(xyz_file, 'r') as file:
-        # Define constants and construct a dictionary: {atomic symbols: [atomic indices]}
         mol_size = get_mol_size(file)
         idx_dict = get_idx_dict(file, mol_size=mol_size, subtract=1)
         file_size = get_file_size(file, add=[2, mol_size])
-        mol_count_float = file_size / (2 + mol_size)
-        mol_count = int(mol_count_float)
 
-        # Check if mol_count_float is fractional; raise an error if it is
-        if mol_count_float - mol_count != 0.0:
-            error = 'A non-integer number of molecules was found in '
-            error += xyz_file + ': ' + str(mol_count)
-            raise IndexError(error)
+    # Check if mol_count_float is fractional; raise an error if it is
+    mol_count = file_size / (2 + mol_size)
+    if not mol_count.is_integer():
+        error = 'A non-integer number of molecules was found in '
+        error += xyz_file + ': ' + str(mol_count)
+        raise IndexError(error)
+    mol_count = int(mol_count)
 
-        # Create an empty (m*n)*3 xyz array
-        shape = mol_count * mol_size, 3
-        xyz = np.empty(shape)
+    # Create an empty (m*n)*3 xyz array
+    shape = mol_count * mol_size, 3
+    xyz = np.empty(shape)
 
     # Fill the xyz array with cartesian coordinates
     with open(xyz_file, 'r') as file:
@@ -49,6 +53,76 @@ def read_multi_xyz(xyz_file, ret_idx_dict=True):
     if ret_idx_dict:
         return xyz, idx_dict
     return xyz
+
+
+def grab_random_slice(xyz_array, probability=0.5):
+    """ Grab and return a random number of 2D slices from **xyz_array** as a new array.
+    Slices are grabbed along axis 0. The new array consists of views of **xyz_array**.
+
+    :parameter xyz_array: A 3D array with cartesian coordinates of *m*
+        molecules consisting of *n* atoms.
+    :type xyz_array: *m*n*3* |np.ndarray|_ [|np.float64|_]
+    :parameter float probability: The probability of returning each 2D slice from **xyz_array**.
+        Accepts values between 0.0 (0%) and 1.0 (100%).
+    :return: A random number, weighted by **probability**, of 2D slices from **xyz_array**.
+    :rtype: *k*n*3* |np.ndarray|_ [|np.float64|_], where *k* ≈ *m* * **probability**.
+    """
+    if probability <= 0.0 or probability >= 1.0:
+        raise IndexError('A probability must be larger than 0.0 and smaller than 1.0')
+    elif len(xyz_array.shape) == 2 or xyz_array.shape[0] == 1:
+        raise IndexError('Grabbing random 2D slices from a 2D array makes no sense')
+
+    size = int(xyz_array.shape[0] * 0.5)
+    idx_range = np.arange(xyz_array.shape[0], dtype=int)
+    idx = np.random.choice(idx_range, size)
+    return xyz_array[idx]
+
+
+def multi_xyz_to_molecule(xyz_array, idx_dict):
+    """ Convert the output of :func:`FOX.functions.read_xyz.read_multi_xyz`, an array and
+    dictionary, into a list of PLAMS molecules.
+
+    :parameter xyz_array: A 3D or 2D array with cartesian coordinates of *m*
+        molecules consisting of *n* atoms.
+    :type xyz_array: *m*n*3* or *n*3* |np.ndarray|_ [|np.float64|_]
+    :parameter idx_dict: A dictionary with the atomic symbols in **xyz_array** as keys
+        and matching atomic indices as values.
+    :type idx_dict: |dict|_ (keys: |str|_, values: |list|_ [|int|_]).
+    :return: A list of PLAMS molecules.
+    :rtype: |list|_ [|plams.Molecule|_].
+    """
+    # Make sure we're dealing with a 3d array
+    if len(xyz_array.shape) == 2:
+        xyz_array = xyz_array[None, :, :]
+
+    # Create a dictionary with atomic indices as keys and matching atomic symbols as values
+    idx_dict = invert_dict(idx_dict)
+
+    # Construct a template molecule
+    mol_template = Molecule()
+    mol_template.properties.frame = 1
+    for i in range(xyz_array.shape[1]):
+        at = Atom(symbol=idx_dict[i])
+        mol_template.add_atom(at)
+
+    # Create copies of the template molecule and update their cartesian coordinates
+    ret = []
+    for i, xyz in enumerate(xyz_array):
+        mol = mol_template.copy()
+        mol.from_array(xyz)
+        mol.properties.frame += i
+        ret.append(mol)
+
+    return ret
+
+
+def invert_dict(dic):
+    """ Take a dictionary and turn keys into values and values into keys. """
+    ret = {}
+    for key in dic:
+        for i in dic[key]:
+            ret[i] = key
+    return ret
 
 
 def get_mol_size(file):
