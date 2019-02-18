732c5665542952a18b74a3dab6156da2f3d046e3
Bas van Beek <b.f.van.beek@vu.nl>
2019-02-18 12:25:39 +0100

    radial_distribution changed into rdf

--- FOX/examples/input.py
+++ FOX/examples/input.py
@@ -5,7 +5,7 @@ __all__ = []
 import time
 
 from FOX.functions.read_xyz import read_multi_xyz
-from FOX.functions.radial_distribution import get_all_radial
+from FOX.functions.rdf import get_all_radial
 from FOX.examples.example_xyz import get_example_xyz
 
 
