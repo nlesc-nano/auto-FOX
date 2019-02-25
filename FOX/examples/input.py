1c75a564b2ae1b386bca133ec078c6a7f44014a7
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2019-02-25 10:14:00 +0100

    Update input.py

--- FOX/examples/input.py
+++ FOX/examples/input.py
@@ -8,25 +8,23 @@ from FOX.functions.multi_mol import MultiMolecule
 from FOX.examples.example_xyz import get_example_xyz
 
 
-# Define constants
-dr = 0.05
-r_max = 12.0
+# Define the atoms of interest and the .xyz path + filename
 atoms = ('Cd', 'Se', 'O')
-xyz_file = get_example_xyz()
+example_xyz_filename = get_example_xyz()
 
-# Start the timer
+# Optional: start the timer
 print('')
 start = time.time()
 
-# Run the actual script
-mol = MultiMolecule(filename=xyz_file)
+# Read the .xyz file
+mol = MultiMolecule(filename=example_xyz_filename)
 
 # Calculate the RDF, RSMF & RMSD
-rdf = mol.init_rdf(dr=dr, r_max=r_max, atom_subset=atoms)
+rdf = mol.init_rdf(atom_subset=atoms)
 rmsf = mol.init_rmsf(atom_subset=atoms)
 rmsd = mol.init_rmsd(atom_subset=atoms)
 
-# Print the results
+# Optional: print the results and try to plot them in a graph (if Matplotlib is installed)
 print('run time:', '%.2f' % (time.time() - start), 'sec')
 try:
     rdf.plot()
