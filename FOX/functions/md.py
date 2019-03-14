8200e7cc1a6eb28a8f8bec300585df319940aa25
Bas van Beek <b.f.van.beek@vu.nl>
2019-03-14 11:33:43 +0100

    Updated MC function

--- FOX/functions/md.py
+++ FOX/functions/md.py
@@ -6,6 +6,7 @@ from os.path import (join, dirname)
 
 import yaml
 import numpy as np
+import pandas as pd
 
 from scm.plams import Molecule
 from scm.plams.core.results import Results
@@ -13,7 +14,6 @@ from scm.plams.core.settings import Settings
 from scm.plams.core.functions import (init, finish, add_to_class)
 from scm.plams.interfaces.thirdparty.cp2k import Cp2kJob
 
-from .radial_distribution import get_all_radial
 from ..classes.multi_mol import MultiMolecule
 
 
@@ -52,44 +52,105 @@ def get_md(job, s):
     return MultiMolecule(filename=xyz_file)
 
 
-def param_in_history(param, param_history):
-    """ Report if a set of parameters has already been visited.
-    Return False it has not, return its index in *param_history* if it has. """
-    try:
-        return np.where(param in param_history)[0][0]
-    except IndexError:
-        return False
-
-
-def get_aux_error(rdf_QM, rdf_MM):
+def get_aux_error(rdf, rdf_ref):
     """ Return the auxiliary error defined as dEps = Eps_QM - Eps_MM.
 
     g_QM & G_MM <np.ndarray>: A m*n numpy arrays of m radial distribution functions of QM & MM
         calculations, respectively.
     return <float>: The auxiliary error dEps.
     """
-    return np.linalg.norm(rdf_QM - rdf_MM, axis=0).sum()
+    return np.linalg.norm(rdf - rdf_ref, axis=0).sum()
 
 
-def init_mc(i, j):
-    # Step 1: Generate a random trial state
-    param = np.random.rand()
+def _sanitize_init_mc(rdf_ref, start_param, M=10000, phi=1.0, gamma=2.0, omega=100, a_target=0.25):
+    """ Sanitize the arguments of :func:`FOX.functions.read_xyz.read_multi_xyz`.
+    See aforementioned function for a description of the various parameters.
 
-    # Step 2: Check if the trial state has already been visited
-    idx = param_in_history(param, param_history)
-    if idx:
-        rdf_new = rdf_history[idx]
-    else:
-        rdf_new = get_new_rdf(mol, job1, s, param)
+    :return: A sanitized version of: start_param, M, omega, phi, gamma & a_target
+    """
+    # Sanitize **rdf_ref**
+    assert isinstance(rdf_ref, (np.ndarray, pd.DataFrame, pd.Series))
 
-    # Step 3: Evaluate the auxilary error
-    rdf_old = rdf_history[j]
-    accept = get_aux_error(rdf_QM, rdf_new) - get_aux_error(rdf_QM, rdf_old)
-    accept *= -1
+    # Sanitize **start_param**
+    assert isinstance(start_param (np.ndarray, int, np.integer))
+    if not isinstance(start_param, np.darray):
+        start_param = np.random.rand(start_param)
 
-    # Step 4: Update
-    phi = None
-    if accept:
-        rdf_history[i] = rdf_new + phi
-    else:
-        rdf_history[j] += phi
+    # Sanitize **M**
+    M = int(M)
+
+    # Sanitize **omega**
+    omega = int(omega)
+    assert (M // omega) > 1
+
+    # Sanitize **phi**
+    phi = float(phi)
+
+    # Sanitize **gamma**
+    gamma = float(gamma)
+
+    # Sanitize **a_target**
+    a_target = float(a_target)
+    assert 0.0 < a_target < 1.0
+
+    return start_param, M, omega, phi, gamma, a_target
+
+
+def run_md(mol, job_type=Cp2kJob, settings=True):
+    return mol.init_rdf()
+
+
+def init_mc(rdf_ref, start_param, M=10000, phi=1.0, gamma=2.0, omega=100, a_target=0.25):
+    """
+    :parameter rdf_ref: A
+    :type rdf_ref: |np.ndarray|_, |pd.DataFrame|_ or |pd.Series|_
+    :parameter start_param: An array with
+    :type start_param: |int|_ or |np.ndarray|_
+    :parameter int M: The total number of iterations.
+    :parameter float phi: The incremenation factor.
+    :parameter float gamma: The incremenation factor.
+    :parameter int omega: Divides the total number of iterations, **M**, into *N* subiteration
+        blocks of length **omega**: *M* = *N*omega*. Should be, at minimum, twice as large as **M**
+    :parameter float a_target: The target acceptance rate.
+    """
+    param, M, omega, phi, gamma, a_target = _sanitize_init_mc(**locals())
+
+    # The acceptance rate
+    a = np.zeros(omega, dtype=bool)
+
+    # Generate the first RDF
+    key = tuple(param)
+    history_dict = {key: mol.run_md() + phi}
+
+    # Start the MC parameter optimization
+    N = (M // omega)
+    for i in range(N):  # Iteration
+        for j in range(omega):  # Sub-iteration
+            # Step 1: Generate a random trial state
+            rng = np.random.choice(len(param), 1)
+            param[rng] += np.random.rand(1) - 0.5
+            key_old = key
+            key = tuple(param)
+
+            # Step 2: Check if the trial state has already been visited
+            if key in history_dict:
+                rdf = history_dict[key]
+            else:
+                rdf = mol.run_md()
+
+            # Step 3: Evaluate the auxilary error
+            rdf_old = history_dict[key_old]
+            accept = get_aux_error(rdf_ref, rdf_old) - get_aux_error(rdf_ref, rdf)
+
+            # Step 4: Update
+            if accept > 0:
+                a[j] = True
+                history_dict[key] = rdf + phi
+            else:
+                history_dict[key_old] += phi
+
+        # Update phi and reset the acceptance rate a
+        phi *= gamma**np.sign(a_target - np.average(a))
+        a[:] = False
+
+    return param
