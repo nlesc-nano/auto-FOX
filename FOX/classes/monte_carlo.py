95cab05cd4034d17db2a43cb7945c8abc1723b85
Bas van Beek <b.f.van.beek@vu.nl>
2019-03-29 15:06:26 +0100

    WiP: MonteCarlo and ARMC class

--- FOX/classes/monte_carlo.py
+++ FOX/classes/monte_carlo.py
@@ -7,7 +7,6 @@ from os.path import (join, dirname, isfile, isdir)
 
 import yaml
 import numpy as np
-import pandas as pd
 
 from scm.plams import Molecule
 from scm.plams.core.results import Results
@@ -22,48 +21,65 @@ from .multi_mol import MultiMolecule
 def get_xyz_path(self):
     """ Return the path + filename to an .xyz file. """
     for file in self.files:
-        if '.xyz' in file:
+        if 'pos.xyz' in file:
             return self[file]
     raise FileNotFoundError()
 
 
 class MonteCarlo():
-    """ The MonteCarlo class. """
-    def __init__(self, param,
-                 move_range=None, move_func=np.add, move_func_kwarg={},
-                 mol=None, job_type=Cp2kJob, settings=None, atom_subset=None, name=None, path=None):
+    """ The base MonteCarlo class. """
+    def __init__(self, param, molecule, ref, **kwarg):
         """ """
+        # Reference PES descriptor(s)
+        self.ref = ref
         self.param = param
 
-        self.move = Settings()
-        self.move.range = move_range
-        self.move.func = move_func
-        self.move.kwarg = move_func_kwarg
+        # Settings for generating PES descriptors
+        self.pes = Settings()
+        self.pes.func = [MultiMolecule.init_rdf]
+        self.pes.kwarg = [{'atom_subset': None}]
 
+        # Settings for generating Monte Carlo moves
+        self.move = Settings()
+        self.move.range = self.set_move_range()
+        self.move.func = np.add
+        self.move.kwarg = {}
+
+        # Settings for running the actual MD calculations
         self.job = Settings()
-        self.job.mol = mol or Molecule()
-        self.job.job_type = job_type
-        self.job.settings = settings or self.get_settings()
-        self.job.name = name or str(job_type).rsplit("'", 1)[0].rsplit('.', 1)[-1]
-        self.job.path = path or os.get_cwd()
+        self.job.molecule = molecule
+        self.job.job_type = Cp2kJob
+        self.job.settings = self.get_settings()
+        self.job.name = str(self.job.job_type).rsplit("'", 1)[0].rsplit('.', 1)[-1]
+        self.job.path = os.get_cwd()
+
+        # Set user-specified keywords
+        for key in kwarg:
+            if key in self.param:
+                self.param[key] = kwarg[key]
+            elif key in self.move:
+                self.move[key] = kwarg[key]
+            elif key in self.job:
+                self.job[key] = kwarg[key]
 
         self._sanitize()
 
     def _sanitize(self):
         """ Sanitize and validate all arguments of __init__(). """
-        # self.job
-        assert isinstance(self.job.mol, (Molecule, MultiMolecule))
-        if isinstance(self.job.mol, MultiMolecule):
-            self.job.mol = self.job.mol.as_Molecule(0)[0]
+        if not isinstance(self.ref, list):
+            self.ref = list(self.ref)
+
+        assert isinstance(self.job.molecule, (Molecule, MultiMolecule))
+        if isinstance(self.job.molecule, MultiMolecule):
+            self.job.molecule = self.job.molecule.as_Molecule(-1)[0]
+
         assert isinstance(self.job.settings, (dict, str))
         if isinstance(self.job.settings, str):
             assert isfile(self.job.settings)
             self.job.settings = self.get_settings(path=self.job.settings)
+
         assert isinstance(self.job.name, str)
         assert isdir(self.job.path)
-
-        # self.rdf
-        assert isinstance(self.rdf.rdf_ref, (np.ndarray, pd.Series, pd.DataFrame))
 
     def get_settings(self, path=None):
         """ Read and return the template with default CP2K MM-MD settings.
@@ -80,14 +96,8 @@ class MonteCarlo():
             self.job.settings = yaml.load(file)
 
     def set_move_range(self, move_max=0.1, move_min=0.005, move_step=0.005):
-        """ Generate an array of all allowed moves. """
-        # Create the move range
-        move_max = move_max or self.mc.move_max
-        move_min = move_min or self.mc.move_min
-        move_step = move_step or self.mc.move_step
+        """ Generate and set an array of all allowed moves. """
         rng_range = np.arange(move_min, move_max + move_min, move_step)
-
-        # Set the move range
         self.move_range = np.concatenate((-rng_range, rng_range))
 
     def move(self):
@@ -98,9 +108,14 @@ class MonteCarlo():
         self.param[i] = self.move.func(self.param[i], j, **self.move.kwarg)
 
     def run_md(self):
-        """ Run a MD job. """
+        """ Run a MD job.
+
+        :return: A MultiMolecule object constructed from the MD trajectory.
+        :rtype: |FOX.MultiMolecule|_
+        """
         # Run an MD calculation
-        job = self.job.job_type(**self.job)
+        job_type = self.job.job_type
+        job = job_type(**self.job)
         results = job.run()
         results.wait()
 
@@ -108,50 +123,22 @@ class MonteCarlo():
         return MultiMolecule(filename=results.get_xyz_path())
 
     def run_first_md(self):
-        """ Run the first MD calculation before starting the main for-loop.
+        """ Run the first MD job before starting the actual main for-loop.
 
-        :return: A dictionary, containing a list with one or more descriptors of the PES, and
-            the first and only key of aforementioned dictionary.
+        :return: A dictionary, containing a list with one or more PES descriptors, and
+            the first (and only) key in aforementioned dictionary.
         :rtype: |dict|_ (keys: |tuple|_, values: |list|_) and |tuple|_
         """
+        # Run MD
         key = tuple(self.param)
         mol = self.run_md()
-        values = [mol.init_rdf(self.rdf.atom_subset)]
+
+        # Create PES descriptors
+        func_list = self.pes.func
+        kwarg_list = self.pes.kwarg
+        values = [func(mol, **kwarg) for func, kwarg in zip(func_list, kwarg_list)]
         history_dict = {key: self.apply_phi(values)}
         return history_dict, key
-
-
-class ARMC(MonteCarlo):
-    """ The Addaptive Rate Monte Carlo (ARMC) class. """
-    def __init__(self, iter_len=50000, sub_iter_len=100, gamma=2.0, a_target=0.25,
-                 phi=1.0, phi_func=np.add, phi_kwarg={},
-                 **kwargs):
-        """ """
-        MonteCarlo.__init__(self, **kwargs)
-
-        # General ARMC settings
-        self.armc = Settings()
-        self.armc.iter_len = int(iter_len)
-        self.armc.sub_iter_len = int(sub_iter_len)
-        self.armc.gamma = gamma
-        self.armc.a_target = a_target
-        assert iter_len / sub_iter_len >= 1.0
-
-        # Settings specific to handling phi
-        self.armc.phi = float(phi)
-        self.armc.phi_func = np.add
-        self.armc.phi_kwarg = phi_kwarg
-
-    def get_aux_error(self, array):
-        """ Return the auxiliary error, defined as dEps = Eps_QM - Eps_MM,
-        between two radial distribution functions.
-
-        :parameter rdf: A radial distribution function.
-        :type array: |np.ndarray|_, |pd.DataFrame|_ or |pd.Series|_
-        :return: The auxiliary error, dEps, between two radial distribution functions.
-        :rtype: |float|_
-        """
-        return np.linalg.norm(array - self.ref, axis=0).sum()
 
     def key_in_history(self, history_dict, key):
         """ Check if a **key** is already present in **history_dict**.
@@ -168,7 +155,43 @@ class ARMC(MonteCarlo):
             return history_dict[key]
         else:
             mol = self.run_md()
-            return mol.init_rdf(self.rdf.atom_subset)
+            func_list = self.pes.func
+            kwarg_list = self.pes.kwarg
+            return [func(mol, **kwarg) for func, kwarg in zip(func_list, kwarg_list)]
+
+
+class ARMC(MonteCarlo):
+    """ The addaptive rate Monte Carlo (ARMC) class. """
+    def __init__(self, **kwarg):
+        """ """
+        MonteCarlo.__init__(self, **kwarg)
+
+        # Settings specific to addaptive rate Monte Carlo (ARMC)
+        self.armc = Settings()
+        self.armc.iter_len = 50000
+        self.armc.sub_iter_len = 100
+        self.armc.gamma = 2.0
+        self.armc.a_target = 0.25
+
+        # Settings specific to handling the phi parameter in ARMC
+        self.armc.phi = 1.0
+        self.armc.phi_func = np.add
+        self.armc.phi_kwarg = {}
+
+        # Set user-specified keywords
+        for key in kwarg:
+            if key in self.armc:
+                self.armc[key] = kwarg[key]
+
+    def get_aux_error(self, values):
+        """ Return the auxiliary error.
+
+        :parameter values: A list of PES descriptors.
+        :type values: |list|_
+        :return: An array of auxilary errors
+        :rtype: |float|_
+        """
+        return np.array([np.linalg.norm(i - j, axis=0).sum() for i, j in zip(values, self.ref)])
 
     def apply_phi(self, values):
         """ Update all values in **values** with **self.amc.phi**.
@@ -183,10 +206,13 @@ class ARMC(MonteCarlo):
             func(i, phi, **kwarg, out=i)
 
     def update_phi(self, acceptance):
-        """ Update **self.armc.phi** and reset all values in **acceptance** to *False*. """
+        """ Update **self.armc.phi** based on **acceptance**.
+
+        :parameter acceptance: An array denoting the accepted moves within a sub-iteration.
+        :type acceptance: |np.ndarray|_ [|bool|_]
+        """
         sign = np.sign(self.armc.a_target - np.mean(acceptance))
         self.armc.phi *= self.armc.gamma**sign
-        acceptance[:] = False
 
     def init_armc(self, ref):
         """ Initialize the Addaptive Rate Monte Carlo procedure.
@@ -199,8 +225,8 @@ class ARMC(MonteCarlo):
         """
         # Unpack attributes
         acceptance = np.zeros(self.armc.sub_iter_len, dtype=bool)
-        sub_iter = self.mc.sub_iter_len
-        super_iter = self.mc.iter_len // sub_iter
+        sub_iter = self.armc.sub_iter_len
+        super_iter = self.armc.iter_len // sub_iter
 
         # Set attributes
         self.set_move_range()
@@ -208,28 +234,29 @@ class ARMC(MonteCarlo):
 
         # Initialize
         init(path=self.job.path, folder='MM-MD')
-        key, history_dict = self.run_first_md()
+        key_new, history_dict = self.run_first_md()
         for _ in range(super_iter):
             for i in range(sub_iter):
                 # Step 1: Perform a random move
-                key_old = key
-                key = tuple(self.param)
+                key_old = key_new
                 self.move()
+                key_new = tuple(self.param)
 
                 # Step 2: Check if the move has been performed already
-                new = self.key_in_history(history_dict, key)
+                value_new = self.key_in_history(history_dict, key_new)
 
                 # Step 3: Evaluate the auxilary error
-                old = history_dict[key_old]
-                accept = bool(self.get_aux_error(old) - self.get_aux_error(new))
+                value_old = history_dict[key_old]
+                accept = bool(sum(self.get_aux_error(value_old) - self.get_aux_error(value_new)))
 
-                # Step 4: Update the rdf history
+                # Step 4: Update the PES descriptor history
                 if accept:
                     acceptance[i] = True
-                    history_dict[key] = self.apply_phi(new)
+                    history_dict[key_new] = self.apply_phi(value_new)
                 else:
-                    history_dict[key_old] = self.apply_phi(old)
-            self.update_phi()  # Update phi and reset the acceptance rate
+                    history_dict[key_old] = self.apply_phi(value_old)
+            self.update_phi(acceptance)
+            acceptance[:] = False
         finish()
 
         return self.param
