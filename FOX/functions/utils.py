7488f7e92fa85a827804dd1de8d5bfa4de0fbf36
Bas van Beek <b.f.van.beek@vu.nl>
2019-04-10 14:18:50 +0200

    3 new functions

--- FOX/functions/utils.py
+++ FOX/functions/utils.py
@@ -78,7 +78,7 @@ def read_str_file(filename):
                 return zip(*inner_loop(f))
 
 
-def _read_param(filename):
+def read_param(filename):
     """ Read a CHARMM parameter file.
 
     :parameter str filename: the path+filename of the CHARMM parameter file.
@@ -103,3 +103,77 @@ def _read_param(filename):
                     break
 
     return Settings(df_dict)
+
+
+def get_shape(item):
+    """ Try to infer the shape of an object.
+
+    :parameter object item: A python object.
+    :return: The shape of **item**.
+    :rtype: |tuple|_ [|int|_]
+    """
+    if hasattr(item, 'shape'):  # Plan A: **item** is an np.ndarray derived object
+        return item.shape
+    elif hasattr(item, '__len__'):  # Plan B: **item** has access to the __len__() magic method
+        return (len(item), )
+    return (1, )  # Plan C: **item** has access to neither A nor B
+
+
+def flatten_dict(input_dict):
+    """ Flatten a dictionary.
+    The keys of the to be returned dictionary consist are tuples with the old (nested) keys
+    of **input_dict**.
+
+    .. code-block:: python
+
+        >>> print(input_dict)
+        {'a': {'b': {'c': True}}}
+
+        >>> output_dict = flatten_dict(input_dict)
+        >>> print(output_dict)
+        {('a', 'b', 'c'): True}
+
+    :parameter dict input_dict: A (nested) dicionary.
+    :return: A non-nested dicionary derived from **input_dict**.
+    :rtype: |dict|_ (keys: |tuple|_)
+    """
+    def flatten(item):
+        ret = {}
+        for i in item:
+            if isinstance(item[i], dict):
+                for j in item[i]:
+                    ret[i + (j, )] = item[i][j]
+            else:
+                ret[i] = item[i]
+
+        if item == ret:
+            return ret, True
+        return ret, False
+
+    # Changes keys into tuples
+    flat_dict = {(key, ): input_dict[key] for key in input_dict}
+
+    # Un-nest and return the input dictionary
+    flat = False
+    while not flat:
+        flat_dict, flat = flatten(flat_dict)
+    return flat_dict
+
+
+def dict_to_pandas(input_dict, name=0, object_type='DataFrame'):
+    """ Turn a (nested) dictionary into a pandas series or dataframe.
+    Keys are un-nested and used for generating multiindices (see meth:`flatten_dict`).
+
+    :parameter dict input_dict: A (nested) dictionary.
+    :parameter object name: The name of the to be returned series/dataframe.
+    :parameter str object_type: The object type of the to be returned item.
+        Accepted values are *Series* or *DataFrame*
+    :return: A pandas series or dataframe created fron **input_dict**.
+    :rtype: |pd.Series|_ or |pd.DataFrame|_ (index: |pd.MultiIndex|_)
+    """
+    flat_dict = flatten_dict(input_dict)
+    idx = pd.MultiIndex.from_tuples(flat_dict.keys())
+    if object_type.lower() == 'series':
+        return pd.Series(list(flat_dict.values()), index=idx, name=name)
+    elif object_type.lower() == 'dataframe':
+        return pd.DataFrame(list(flat_dict.values()), index=idx, columns=[name])
