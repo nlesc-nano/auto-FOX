4c07e3202e4f42fceab26631d8db10d57d4b67e8
Bas van Beek <b.f.van.beek@vu.nl>
2019-04-16 13:23:33 +0200

    Update test_shell.py

--- FOX/examples/test_shell.py
+++ FOX/examples/test_shell.py
@@ -1,35 +1,12 @@
-import FOX
-
 from FOX.classes.multi_mol import MultiMolecule
 from FOX.examples.example_xyz import get_example_xyz
+import matplotlib.pyplot as plt
 
-import numpy as np
-import pandas as pd
+example_xyz_file = get_example_xyz()
+mol = MultiMolecule(filename=example_xyz_file)
+rmsf, rmsf_idx, rdf = mol.init_shell_search(atom_subset=('Cd', 'Se'))
 
-
-atoms = ('Cd', 'Se')
-mol = MultiMolecule(filename=get_example_xyz())
-dist_mean = []
-mol2 = mol.deepcopy()
-for at in atoms:
-    mol2 -= np.average(mol2[:, mol2.atoms[at]], axis=1)[:, None, :]
-    dist = np.linalg.norm(mol2.coords[:, mol2.atoms[at]], axis=2).mean(axis=0)
-    dist.sort()
-    dist_mean.append(dist)
-
-index = np.arange(0, mol.shape[1])
-columns, data = mol2._get_rmsf_columns(dist_mean, index, loop=True, atom_subset=atoms)
-df = pd.DataFrame(data=data, index=index, columns=columns)
-df.columns.name = 'Distance from center\n  /  Ångström'
-df.index.name = 'Arbitrary atomic index'
-# df.plot()
-
-mol -= np.average(mol[:, mol.atoms[atoms[-1]]], axis=1)[:, None, :]
-b = np.zeros_like(mol.coords[:, 0, :])[:, None, :]
-mol.coords = np.hstack((mol.coords, b))
-mol.atoms['dummy'] = [mol.shape[1] - 1]
-atoms = ('dummy', ) + atoms
-rdf = mol.init_rdf(atoms)
-del rdf['dummy dummy']
-rdf = rdf.loc[rdf.index >= 0.5, [i for i in rdf.columns if 'dummy' in i]]
-rdf.plot()
+fig, (ax, ax2) = plt.subplots(ncols=2)
+rmsf.plot(ax=ax)
+rdf.plot(ax=ax2)
+plt.show()
