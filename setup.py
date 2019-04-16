596661be34887dfabdb80f46428ecf0e7ce92fe0
Bas van Beek <b.f.van.beek@vu.nl>
2019-04-16 15:06:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'python-3.7',
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Chemistry'
         'License :: OSI Approved :: GNU Lesser General Public License',
