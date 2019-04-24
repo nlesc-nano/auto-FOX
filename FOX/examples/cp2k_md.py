e4a5bfdd9f4a17cd823555d819fc930e49463fea
Bas van Beek <b.f.van.beek@vu.nl>
2019-04-24 13:17:53 +0200

    Update cp2k_md.py

--- FOX/examples/cp2k_md.py
+++ FOX/examples/cp2k_md.py
@@ -1,6 +1,6 @@
 """ A work in progress recipe for MM-MD parameter optimizations with CP2K. """
 
-import FOX
+import os
 from os.path import join
 
 import pandas as pd
@@ -15,15 +15,13 @@ from FOX.functions.utils import (get_template, dict_to_pandas, template_to_df)
 from FOX.functions.cp2k_utils import (set_subsys_kind, set_lennard_jones, set_atomic_charges)
 
 
-"""
 @add_to_class(Cp2kJob)
 def get_runscript(self):
     return 'cp2k.ssmp -i {} -o {}'.format(self._filename('inp'), self._filename('out'))
-"""
 
 
 # Read the .xyz file
-path = '/Users/bvanbeek/Downloads'
+path = '/Users/basvanbeek/Downloads'
 mol = MultiMolecule(filename=get_example_xyz())
 mol.guess_bonds(atom_subset=['C', 'O', 'H'])
 mol._set_psf_block()
@@ -62,16 +60,23 @@ charge_constrain.OG2D2 = {'Cd': -2, 'Se': 2}
 # Prepare the ARMC settings
 carlos = ARMC(param, mol)
 carlos.job.path = path
+carlos.job.settings = s
+carlos.job.settings.input.motion.md.max_steps //= 100
+carlos.job.settings.input.motion.md.steps //= 100
+carlos.job.settings.input.motion.md.time_start_val //= 100
+carlos.job.settings.input.motion.md.timestep *= 100
 carlos.job.keep_files = True
 carlos.job.charge_series = df['charge'].copy()
 carlos.job.charge_series.index = df['atom type']
 carlos.pes.rdf.func = MultiMolecule.init_rdf
 carlos.pes.rdf.kwarg = {'atom_subset': ('Cd', 'Se', 'O')}
 carlos.pes.rdf.ref = mol.init_rdf(**carlos.pes.rdf.kwarg)
-carlos.job.settings = s
 carlos.armc.iter_len = 12
 carlos.armc.sub_iter_len = 3
-carlos.hdf5_path = '/Users/bvanbeek/Documents'
+carlos.hdf5_path = path
 
-FOX.create_hdf5(carlos, path)
-#carlos.init_armc()
+try:
+    os.remove(join(path, 'MC.hdf5'))
+except FileNotFoundError:
+    pass
+carlos.init_armc()
