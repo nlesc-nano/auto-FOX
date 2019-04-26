1ad4ebdb09066ffa7cfdb279e7957110fae943d5
Bas van Beek <b.f.van.beek@vu.nl>
2019-04-26 18:10:40 +0200

    Update test_example.py

--- test/test_example.py
+++ test/test_example.py
@@ -2,7 +2,7 @@
 
 __all__ = []
 
-from os.path import (join, isfile)
+from os.path import join
 
 import numpy as np
 
@@ -14,15 +14,16 @@ REF_DIR = 'test/test_files'
 
 def test_input():
     """ Test :mod:`FOX.examples.input`. """
+    global_dict = {}
+    local_dict = {}
     path = join(FOX.__path__[0], 'examples/input.py')
-    assert isfile(path)
     with open(path, 'r') as f:
-        exec(f.read())
+        exec(f.read(), global_dict, local_dict)
 
     ref_rdf = np.load(join(REF_DIR, 'rdf.npy'))
     ref_rmsf = np.load(join(REF_DIR, 'rmsf.npy'))
     ref_rmsd = np.load(join(REF_DIR, 'rmsd.npy'))
 
-    np.testing.assert_allclose(rdf.values, ref_rdf)
-    np.testing.assert_allclose(rmsf.values, ref_rmsf)
-    np.testing.assert_allclose(rmsd.values, ref_rmsd)
+    np.testing.assert_allclose(local_dict['rdf'].values, ref_rdf)
+    np.testing.assert_allclose(local_dict['rmsf'].values, ref_rmsf)
+    np.testing.assert_allclose(local_dict['rmsd'].values, ref_rmsd)
