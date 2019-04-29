bf711e18f1c72c7f4153bcff01775c8b523f6a30
Bas van Beek <b.f.van.beek@vu.nl>
2019-04-29 16:39:25 +0200

    Minor style change

--- FOX/examples/cp2k_md.py
+++ FOX/examples/cp2k_md.py
@@ -1,12 +1,16 @@
 """ A Recipe for MM-MD parameter optimizations with CP2K 3.1 or 6.1. """
 
+from os.path import join
+
+import FOX
 from FOX import (ARMC, MultiMolecule, get_template, get_example_xyz)
 
 
 # Read the .xyz file and generate
+str_file = join(FOX.__path__[0], 'data/formate.str')
 mol = MultiMolecule.from_xyz(get_example_xyz())
 mol.guess_bonds(atom_subset=['C', 'O', 'H'])
-mol.update_atom_type('formate.str')
+mol.update_atom_type(str_file)
 mol.update_atom_charge('Cd', 2.0)
 mol.update_atom_charge('Se', -2.0)
 psf = mol.as_psf(return_blocks=True)
@@ -18,4 +22,4 @@ s.job.molecule = mol
 
 # Start the MC parameterization
 armc = ARMC.from_dict(s)
-armc.init_armc()
+# armc.init_armc()
