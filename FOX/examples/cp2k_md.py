9495e036ff0d3b4119cc736b5b0bb46f3afed01c
Bas van Beek <b.f.van.beek@vu.nl>
2019-05-08 22:57:39 +0200

    Renamed formate.* to ligand.*

--- FOX/examples/cp2k_md.py
+++ FOX/examples/cp2k_md.py
@@ -6,21 +6,6 @@ import FOX
 from FOX import (ARMC, MultiMolecule, get_template, get_example_xyz)
 
 
-# Read the .xyz file and generate
-str_file = join(FOX.__path__[0], 'data/formate.str')
-mol = MultiMolecule.from_xyz(get_example_xyz())
-mol.guess_bonds(atom_subset=['C', 'O', 'H'])
-mol.update_atom_type(str_file)
-mol.update_atom_charge('Cd', 0.9768)
-mol.update_atom_charge('Se', -0.9768)
-psf = mol.as_psf(return_blocks=True)
-
-# Prepare the ARMC settings
-path = join(FOX.__path__[0], 'examples')
-s = get_template('armc.yaml', path)
-s.job.psf = psf
-s.job.molecule = mol
-
 # Start the MC parameterization
-armc = ARMC.from_dict(s)
+armc = ARMC.from_yaml('armc.yaml')
 # armc.init_armc()
