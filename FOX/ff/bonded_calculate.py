Fixed incorrect axis

FOX/ff/bonded_calculate.py
@@ -256,12 +256,12 @@ def get_V_dihedrals(df: pd.DataFrame, mol: MultiMolecule, dihed_idx: np.ndarray)
     """
     symbol = mol.symbol
     dihedral = _dihed(mol, dihed_idx)
-    ret = pd.DataFrame(0.0, index=pd.RangeIndex(0, len(mol), name='au'), columns=df.index)
 
     # Remove duplicate indices
     # The .prm file format allows for multiple declaration of dihedral parameters,
     # resulting in duplicate multi-indices
-    ret = ret.loc[~ret.index.duplicated(keep='first')]
+    columns = df.index[~df.index.duplicated(keep='first')]
+    ret = pd.DataFrame(0.0, index=pd.RangeIndex(0, len(mol), name='au'), columns=columns)
 
     iterator = df.iloc[:, 0:3].iterrows()
     for i, item in iterator: