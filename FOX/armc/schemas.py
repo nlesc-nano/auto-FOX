d5e8e8b1063a87ee7bf1c8b8c44155afa7d96fb1
Bas van Beek <b.f.van.beek@vu.nl>
2020-04-03 20:28:20 +0200

    Added explicit error message to the schemas

--- FOX/armc/schemas.py
+++ FOX/armc/schemas.py
@@ -136,33 +136,39 @@ phi_schema = Schema({
     Optional_('type', default=lambda: PhiUpdater): Or(
         And(None, Default(PhiUpdater)),
         And(str, Use(lambda n: get_importable(n, validate=phi_subclass))),
-        And(type, phi_subclass)
+        And(type, phi_subclass),
+        error="'phi.type' expected a PhiUpdater class; observed value: {!r:100}"
     ),
 
     Optional_('phi', default=1.0): Or(
         And(None, Default(1.0)),
         And(supports_float, Use(float)),
+        error="'phi.phi' expected a float; observed value: {!r:100}"
     ),
 
     Optional_('gamma', default=2.0): Or(
         And(None, Default(2.0)),
         And(supports_float, Use(float)),
+        error="'phi.gamma' expected a float; observed value: {!r:100}"
     ),
 
     Optional_('a_target', default=0.25): Or(
         And(None, Default(0.25)),
         And(supports_float, lambda n: 0 < float(n) <= 1, Use(float)),
+        error="'phi.a_target' expected a float in the (0, 1] interval; observed value: {!r:100}"
     ),
 
     Optional_('func', default=lambda: np.add): Or(
         And(None, Default(np.add, call=False)),
         And(str, Use(lambda n: get_importable(n, validate=callable))),
-        And(abc.Callable)
+        abc.Callable,
+        error="'phi.func' expected a Callable object; observed value: {!r:100}"
     ),
 
     Optional_('kwargs', default=dict): Or(
         And(None, Default(dict)),
-        And(abc.Mapping, lambda n: all(isinstance(k, str) for k in n.keys()))
+        And(abc.Mapping, lambda n: all(isinstance(k, str) for k in n.keys())),
+        error="'phi.kwargs' expected a Mapping; observed value: {!r:100}"
     )
 })
 
@@ -199,46 +205,54 @@ mc_schema = Schema({
     Optional_('type', default=lambda: ARMC): Or(
         And(None, Default(ARMC, call=False)),
         And(str, Use(lambda n: get_importable(n, validate=mc_subclass))),
-        And(type, mc_subclass)
+        And(type, mc_subclass),
+        error="'monte_carlo.type' expected an ARMC class; observed value: {!r:100}"
     ),
 
     Optional_('iter_len', default=50000): Or(
         And(None, Default(50000)),
-        And(supports_int, lambda n: int(n) > 0, Use(int))
+        And(supports_int, lambda n: int(n) > 0, Use(int)),
+        error="'monte_carlo.iter_len' expected a positive integer; observed value: {!r:100}"
     ),
 
     Optional_('sub_iter_len', default=100): Or(
         And(None, Default(100)),
-        And(supports_int, lambda n: int(n) > 0, Use(int))
+        And(supports_int, lambda n: int(n) > 0, Use(int)),
+        error="'monte_carlo.sub_iter_len' expected a positive integer; observed value: {!r:100}"
     ),
 
     Optional_('hdf5_file', default='armc.hdf5'): Or(
         And(None, Default('armc.hdf5')),
         str,
-        os.PathLike
+        os.PathLike,
+        error="'monte_carlo.hdf5_file' expected a path-like object; observed value: {!r:100}"
     ),
 
     Optional_('logfile', default=lambda: 'armc.log'): Or(
         And(None, Default('armc.log')),
         str,
-        os.PathLike
+        os.PathLike,
+        error="'monte_carlo.logfile' expected a path-like object; observed value: {!r:100}"
     ),
 
     Optional_('path', default=lambda: os.getcwd()): Or(
         And(None, Default(os.getcwd)),
         And(str, Use(os.path.abspath)),
-        And(os.PathLike, Use(os.path.abspath))
+        And(os.PathLike, Use(os.path.abspath)),
+        error="'monte_carlo.path' expected a path-like object; observed value: {!r:100}"
     ),
 
     Optional_('folder', default='MM_MD_workdir'): Or(
         And(None, Default('MM_MD_workdir')),
         str,
-        os.PathLike
+        os.PathLike,
+        error="'monte_carlo.folder' expected a path-like object; observed value: {!r:100}"
     ),
 
     Optional_('keep_files', default=True): Or(
         And(None, Default(True)),
-        bool
+        bool,
+        error="'monte_carlo.keep_files' expected a boolean; observed value: {!r:100}"
     )
 })
 
@@ -275,27 +289,31 @@ psf_schema = Schema({
         None,
         And(str, Use(lambda n: (n,))),
         And(os.PathLike, Use(lambda n: (n,))),
-        And(abc.Sequence, lambda n: all(isinstance(i, (os.PathLike, str)) for i in n), Use(tuple))
+        And(abc.Sequence, lambda n: all(isinstance(i, (os.PathLike, str)) for i in n), Use(tuple)),
+        error="'psf.str_file' expected None or one or more path-like objects; observed value: {!r:100}"
     ),
 
     Optional_('rtf_file', default=None): Or(
         None,
         And(str, Use(lambda n: (n,))),
         And(os.PathLike, Use(lambda n: (n,))),
-        And(abc.Sequence, lambda n: all(isinstance(i, (os.PathLike, str)) for i in n), Use(tuple))
+        And(abc.Sequence, lambda n: all(isinstance(i, (os.PathLike, str)) for i in n), Use(tuple)),
+        error="'psf.rtf_file' expected None or one or more path-like objects; observed value: {!r:100}"
     ),
 
     Optional_('psf_file', default=None): Or(
         None,
         And(str, Use(lambda n: (n,))),
         And(os.PathLike, Use(lambda n: (n,))),
-        And(abc.Sequence, lambda n: all(isinstance(i, (os.PathLike, str)) for i in n), Use(tuple))
+        And(abc.Sequence, lambda n: all(isinstance(i, (os.PathLike, str)) for i in n), Use(tuple)),
+        error="'psf.psf_file' expected None or one or more path-like objects; observed value: {!r:100}"
     ),
 
     Optional_('ligand_atoms', default=None): Or(
         None,
         And(str, Use(lambda n: frozenset({n}))),
-        And(abc.Collection, lambda n: all(isinstance(i, str) for i in n), Use(frozenset))
+        And(abc.Collection, lambda n: all(isinstance(i, str) for i in n), Use(frozenset)),
+        error="'psf.ligand_atoms' expected None or a Collection of atoms; observed value: {!r:100}"
     ),
 })
 
@@ -322,7 +340,8 @@ class PSFDict(TypedDict):
 pes_schema = Schema({
     'func': Or(
         And(str, Use(lambda n: get_importable(n, validate=callable))),
-        abc.Callable
+        abc.Callable,
+        error="'pes.*.func' expected a callable object; observed value: {!r:100}"
     ),
 
     Optional_('kwargs', default=dict): Or(
@@ -333,7 +352,8 @@ pes_schema = Schema({
             lambda n: all(isinstance(i, abc.Mapping) for i in n),
             lambda n: all(isinstance(k, str) for dct in n for k in dct.keys()),
             Use(tuple)
-        )
+        ),
+        error="'pes.*.kwargs' expected a Mapping or Sequence of Mappings; observed value: {!r:100}"
     )
 })
 
@@ -365,7 +385,8 @@ job_schema = Schema({
     Optional_('type', default=lambda: PackageManager): Or(
         And(None, Default(PackageManager, call=False)),
         And(str, Use(lambda n: get_importable(n, validate=pkg_subclass))),
-        And(type, pkg_subclass)
+        And(type, pkg_subclass),
+        error="'job.func' expected a PackageManager class; observed value: {!r:100}"
     ),
 
     'molecule': Or(
@@ -378,7 +399,8 @@ job_schema = Schema({
             Use(lambda n: tuple(
                 (i if isinstance(i, MultiMolecule) else MultiMolecule.from_xyz(i)) for i in n
             ))
-        )
+        ),
+        error="'job.molecule' expected one or more .xyz files; observed value: {!r:100}"
     )
 })
 
@@ -412,18 +434,21 @@ sub_job_schema = Schema({
     Optional_('type', default=lambda: cp2k_mm): Or(
         And(None, Default(cp2k_mm, call=False)),
         And(str, Use(lambda n: get_importable(n, validate=qm_pkg_instance))),
-        Package
+        Package,
+        error="'job.*.type' expected a Package instance; observed value: {!r}"
     ),
 
     Optional_('settings', default=QmSettings): Or(
         And(None, Default(QmSettings)),
-        And(abc.Mapping, Use(QmSettings))
+        And(abc.Mapping, Use(QmSettings)),
+        error="'job.*.settings' expected a Mapping; observed value: {!r}"
     ),
 
     Optional_('template', default=QmSettings): Or(
         And(None, Default(QmSettings)),
         And(str, Use(lambda n: QmSettings(get_importable(n, validate=mapping_instance)))),
-        And(abc.Mapping, Use(QmSettings))
+        And(abc.Mapping, Use(QmSettings)),
+        error="'job.*.template' expected a Mapping; observed value: {!r}"
     )
 })
 
@@ -464,18 +489,21 @@ param_schema = Schema({
     Optional_('type', default=lambda: ParamMapping): Or(
         And(None, Default(ParamMapping, call=False)),
         And(str, Use(lambda n: get_importable(n, validate=prm_subclass))),
-        And(type, prm_subclass)
+        And(type, prm_subclass),
+        error="'param.type' expected a ParamMapping class; observed value: {!r}"
     ),
 
     Optional_('func', default=lambda: np.multiply): Or(
         And(None, Default(np.multiply, call=False)),
         And(str, Use(lambda n: get_importable(n, callable))),
-        abc.Callable
+        abc.Callable,
+        error="'param.func' expected a Callable object; observed value: {!r}"
     ),
 
     Optional_('kwargs', default=dict): Or(
         And(None, Default(dict)),
         And(abc.Mapping, lambda n: all(isinstance(k, str) for k in n.keys())),
+        error="'param.kwargs' expected a Mapping; observed value: {!r}"
     ),
 
     Optional_('move_range', default=lambda: MOVE_DEFAULT.copy()): Or(
@@ -484,7 +512,8 @@ param_schema = Schema({
         And(SupportsArray, Use(lambda n: np.asarray(n, dtype=float))),
         And(abc.Iterable, Use(lambda n: np.fromiter(n, dtype=float))),
         And(abc.Mapping, lambda n: {'start', 'stop', 'step'}.issuperset(n.keys()),
-            Use(lambda n: _get_move_range(**n)))
+            Use(lambda n: _get_move_range(**n))),
+        error="'param.move_range' expected a Mapping or array-like object; observed value: {!r}"
     )
 
 })
