31a7e1f2a80c28636bfba2279f035e027f31d56d
Bas van Beek <b.f.van.beek@vu.nl>
2020-04-04 16:54:45 +0200

    Moved a script

--- FOX/examples/private/test_armc.py
+++ FOX/examples/private/test_armc.py
@@ -5,13 +5,13 @@ from FOX.armc.sanitization import dict_to_armc
 from FOX.armc.run_armc import run_armc
 
 
-file = '/Users/bvanbeek/Documents/GitHub/auto-FOX/FOX/examples/private/armc_new.yaml'
+file = 'armc_new.yaml'
 with open(file, 'r') as f:
     dct = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 
 try:
-    shutil.rmtree('/Users/bvanbeek/Downloads/MM_MD_workdir')
+    shutil.rmtree('MM_MD_workdir')
 except:
     pass
 
