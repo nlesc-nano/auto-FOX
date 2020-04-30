3b71bc68ee02daf1fdf81f20abbaa793f2fb65a6
Bas van Beek <b.f.van.beek@vu.nl>
2020-04-30 11:59:42 +0200

    WiP: Reintroduction of the parameter guessing scheme

--- FOX/armc/guess.py
+++ FOX/armc/guess.py
@@ -7,7 +7,7 @@ A module with functions for guessing ARMC parameters.
 """
 
 import reprlib
-from typing import Union, Iterable, Mapping, Tuple, List, Collection, Optional, Dict, Set, TYPE_CHECKING
+from typing import Union, Iterable, Mapping, Tuple, List, Collection, Optional, Dict, Set, TYPE_CHECKING, Sequence
 from itertools import chain
 
 import numpy as np  # type: ignore
@@ -44,26 +44,31 @@ Mode = Literal[
     'crystal_radii'
 ]
 
+ION_SET = frozenset({'ionic_radius', 'ion_radius', 'ionic_radii', 'ion_radii'})
+CRYSTAL_SET = frozenset({'crystal_radius', 'crystal_radii'})
 
-def guess_param(mol_list: Iterable[MultiMolecule], settings: Settings,
+
+def guess_param(mol_list: Sequence[MultiMolecule], settings: Settings,
                 mode: Mode = 'rdf', param: Param = 'epsilon',
                 prm: Union[None, str, bytes, PathLike, PRMContainer] = None,
-                psf_list: Optional[Iterable[Union[str, bytes, PathLike, PSFContainer]]] = None
+                psf_list: Optional[Sequence[Union[str, bytes, PathLike, PSFContainer]]] = None
                 ) -> Dict[Tuple[str, str], float]:
     # Construct a set with all valid atoms types
-    if psf_list is None:
-        atoms: Set[str] = set(chain.from_iterable(mol.keys() for mol in mol_list))
-    else:
-        atoms = set()
-        for _psf in psf_list:
-            psf: PSFContainer = PSFContainer.read(_psf) if not isinstance(_psf, PSFContainer) else _psf
-            atoms.update(psf.to_atom_dict().keys())
+    if psf_list is not None:
+        for mol, p in zip(mol_list, psf_list):
+            psf: PSFContainer = PSFContainer.read(p) if not isinstance(p, PSFContainer) else p
+            mol.atoms = psf.to_atom_dict()
+    atoms: Set[str] = set(chain.from_iterable(mol.atoms.keys() for mol in mol_list))
 
     # Construct a DataFrame and update it with all available parameters
     df = LJDataFrame(np.nan, index=atoms)
     df.overlay_cp2k_settings(settings)
     if prm is not None:
-        df.overlay_prm(prm)
+        prm_: PRMContainer = prm if isinstance(prm, PRMContainer) else PRMContainer.read(prm)
+        df.overlay_prm(prm_)
+        prm_dict = _process_prm(prm_, param=param)
+    else:
+        prm_dict = {}
 
     # Extract the relevant parameter Series
     _series = df[param]
@@ -71,13 +76,13 @@ def guess_param(mol_list: Iterable[MultiMolecule], settings: Settings,
 
     mode = mode.lower()  # type: ignore
     if mode == 'rdf':
-        rdf(...)
+        rdf(series, mol_list)
     elif mode == 'uff':
-        uff(series, series.loc)
-    elif mode in {'ionic_radius', 'ion_radius', 'ionic_radii', 'ion_radii'}:
-        ion_radius(series, series.loc)
-    elif mode in {'crystal_radius', 'crystal_radii'}:
-        crystal_radius(series, series.loc)
+        uff(series, prm_dict)
+    elif mode in ION_SET:
+        ion_radius(series, prm_dict)
+    elif mode in CRYSTAL_SET:
+        crystal_radius(series, prm_dict)
     else:
         raise ValueError(f"'mode' is of invalid value: {mode!r:.100};\naccepted values: "
                          "'rdf', 'uff', 'crystal_radius' and 'ion_radius'")
@@ -140,3 +145,13 @@ def _set_radii(series: pd.Series,
             value_j = ref_mapping[j]
 
         series[i, j] = func(value_i, value_j)
+
+
+def _process_prm(prm: PRMContainer, param: Param) -> Dict[str, float]:
+    r"""Extract a dict from **prm** with all :math:`\varepsilon` or :math:`\sigma` values."""
+    if prm.nonbonded is None:
+        return {}
+    nonbonded = prm.nonbonded[[2, 3]].copy()
+    nonbonded.columns = ['epsilon', 'sigma']  # kcal/mol and Angstrom
+    nonbonded['sigma'] *= 2 / 2**(1/6)  # Conversion factor between (R / 2) and sigma
+    return nonbonded[param].as_dict()
