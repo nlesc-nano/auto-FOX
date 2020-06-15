f74adddc7eee0921a9dae13e0c4f236d7afebeea
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-15 12:21:28 +0200

    Fixed a few `PRMContainer` issues

--- tests/test_prm.py
+++ tests/test_prm.py
@@ -1,13 +1,15 @@
 """Tests for :class:`nanoCAT.ff.prm.PRMContainer`."""
 
 import os
+import pickle
 from pathlib import Path
 from tempfile import TemporaryFile
 from itertools import zip_longest
 
+import pandas as pd
+
 from assertionlib import assertion
 from nanoutils import delete_finally
-
 from FOX import PRMContainer
 
 PATH = Path('tests') / 'test_files'
@@ -15,6 +17,7 @@ PATH_PRM = PATH / 'Butoxide.prm'
 PATH_TMP = PATH / 'tmp.prm'
 
 PRM = PRMContainer.read(PATH_PRM)
+PRM._pd_printoptions['display.max_rows'] = 10
 
 if os.name == 'nt':
     STRIP = b'\r\n'
@@ -35,3 +38,37 @@ def test_write() -> None:
         f4.seek(0)
         for i, j in zip_longest(f3, f4, fillvalue=b'<FILLVALUE>'):
             assertion.eq(i.rstrip(STRIP), j.rstrip(STRIP))
+
+
+def is_view(df1: pd.DataFrame, df2: pd.DataFrame) -> bool:
+    """Check if all series in **df1** are views of their counterpart in **df2**."""
+    iterator = ((v.values.base, df2[k].values.base) for k, v in df1.items())
+    return all([ar1 is ar2 for ar1, ar2 in iterator])
+
+
+def test_magic() -> None:
+    """Tests for :class:`PRMContainer` magic methods."""
+    # Test __eq__
+    prm_shallow = PRM.copy(deep=False)
+    prm_deep = PRM.copy(deep=True)
+    assertion.eq(PRM, prm_shallow)
+    assertion.eq(PRM, prm_deep)
+
+    # Test copy, __deepcopy__ and __copy__
+    attr_names = PRMContainer.__slots__[:-2]
+
+    iterator1 = ((k, getattr(prm_deep, k), getattr(PRM, k)) for k in attr_names)
+    for name, attr1, attr2 in iterator1:
+        if isinstance(attr1, pd.DataFrame):
+            assertion.assert_(is_view, attr1, attr2, invert=True, message=name)
+
+    iterator2 = ((k, getattr(prm_shallow, k), getattr(PRM, k)) for k in attr_names)
+    for name, attr1, attr2 in iterator2:
+        if isinstance(attr1, pd.DataFrame):
+            assertion.assert_(is_view, attr1, attr2, message=name)
+
+    # Test __reduce__ and __setstate__
+    dumps = pickle.dumps(PRM)
+    loads = pickle.loads(dumps)
+    assertion.eq(PRM, loads)
+    assertion.eq(PRM._pd_printoptions, loads._pd_printoptions)
