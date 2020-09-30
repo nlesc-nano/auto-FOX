nlesc-nano/auto-FOX

commit f3f68726f9dd048e93addd5701ee099c83471b7c

Auto-FOX 0.8.7 (#124)
Implementation of https://github.com/nlesc-nano/auto-FOX/issues/121:
* Allow users to apply more complex charge constraints.
* Store frozen parameters in `ParamMapping["param"]`; mark them via the new `ParamMapping["constant"]` series.
* Made``ChargeError`` pickleable.

FOX/armc/sanitization.py
@@ -16,7 +16,7 @@ import os
 import copy
 from pathlib import Path
 from itertools import islice
-from collections import abc
+from collections import abc, Counter
 from typing import (
     Union, Iterable, Tuple, Optional, Mapping, Any, MutableMapping, Hashable,
     Dict, TYPE_CHECKING, Generator, List, Collection, TypeVar, overload, cast
@@ -42,7 +42,7 @@ from ..io.read_psf import PSFContainer, overlay_str_file, overlay_rtf_file
 from ..classes import MultiMolecule
 from ..functions.cp2k_utils import UNIT_MAP
 from ..functions.molecule_utils import fix_bond_orders, residue_argsort
-from ..functions.charge_utils import assign_constraints
+from ..functions.charge_parser import assign_constraints
 
 if TYPE_CHECKING:
     from .package_manager import PackageManager, PkgDict
@@ -100,6 +100,7 @@ def dict_to_armc(input_dict: MainMapping) -> Tuple[MonteCarloABC, RunDict]:
     psf_list: Optional[List[PSFContainer]] = get_psf(dct['psf'], mol_list)
     run_kwargs['psf'] = psf_list
     update_count(param, psf=psf_list, mol=mol_list)
+    _parse_ligand_alias(psf_list, prm=param)
     if psf_list is not None:
         mc.pes_post_process = [AtomsFromPSF.from_psf(*psf_list)]
         workdir = Path(run_kwargs['path']) / run_kwargs['folder']
@@ -110,11 +111,18 @@ def dict_to_armc(input_dict: MainMapping) -> Tuple[MonteCarloABC, RunDict]:
         _guess_param(mc, _param_frozen, frozen=True, psf=psf_list)
     _guess_param(mc, _param, frozen=False, psf=psf_list)
 
+    mc.param['param'].sort_index(inplace=True)
+    mc.param['param_old'].sort_index(inplace=True)
+    mc.param['min'].sort_index(inplace=True)
+    mc.param['max'].sort_index(inplace=True)
+    mc.param['count'].sort_index(inplace=True)
+    mc.param['constant'].sort_index(inplace=True)
+    mc.param._data['constant'] = mc.param['constant'].astype(bool, copy=False)
+
     # Add PES evaluators
     pes = get_pes(dct['pes'])
     for name, kwargs in pes.items():
         mc.add_pes_evaluator(name, **kwargs)
-
     return mc, run_kwargs
 
 
@@ -142,24 +150,18 @@ def _guess_param(mc: MonteCarloABC, prm: dict,
 
     # Update the constant parameters
     package.update_settings(seq, new_keys=True)
-    if frozen:
-        return
 
     # Update the variable parameters
     param_mapping = mc.param
     for k, v in seq:
-        param = v['param']
-        for atom, value in v.items():
-            if atom == 'param':
-                continue
-            key = (k, param, atom)
-
+        iterator = (((k, v['param'], at), value) for at, value in v.items() if at == 'param')
+        for key, value in iterator:
             param_mapping['param'].loc[key] = value
             param_mapping['param_old'].loc[key] = value
             param_mapping['min'][key] = -np.inf
             param_mapping['max'][key] = np.inf
-            param_mapping['constraints'][key] = None
             param_mapping['count'][key] = 0
+            param_mapping['constant'][key] = frozen
     return
 
 
@@ -224,16 +226,25 @@ def get_param(dct: ParamMapping_) -> Tuple[ParamMapping, dict, dict]:
     _sub_prm_dict = split_dict(
         _prm_dict, preserve_order=True, keep_keys={'type', 'move_range', 'func', 'kwargs'}
     )
+    _sub_prm_dict_frozen = _get_prm_frozen(_sub_prm_dict)
 
     prm_dict = validate_param(_prm_dict)
     kwargs = prm_dict.pop('kwargs')
     data = _get_param_df(_sub_prm_dict)
-    data[['constraints', 'min', 'max']] = list(_get_prm_constraints(_sub_prm_dict))
+    constraints, min_max = _get_prm_constraints(_sub_prm_dict)
+    data[['min', 'max']] = min_max
 
-    _sub_prm_dict_frozen = _get_prm_frozen(_sub_prm_dict)
+    for *_key, value in _get_prm(_sub_prm_dict_frozen):
+        key = tuple(_key)
+        data.loc[key, :] = [value, True, -np.inf, np.inf]
+    data.sort_index(inplace=True)
 
     param_type = prm_dict.pop('type')  # type: ignore
-    return param_type(data, **prm_dict, **kwargs), _sub_prm_dict, _sub_prm_dict_frozen
+    return (
+        param_type(data, constraints=constraints, **prm_dict, **kwargs),
+        _sub_prm_dict,
+        _sub_prm_dict_frozen,
+    )
 
 
 def get_pes(dct: Mapping[str, PESMapping]) -> Dict[str, PESDict]:
@@ -355,7 +366,7 @@ def _psf_idx_iterator(job_list: Iterable[T], phi: Iterable) -> Generator[Tuple[i
             yield i, job
 
 
-def _update_psf_settings(job_lists: Iterable[Iterable[dict]], phi: Iterable,
+def _update_psf_settings(job_lists: Iterable[Iterable[MutableMapping[str, Any]]], phi: Iterable,
                          workdir: Union[str, os.PathLike]) -> None:
     """Set the .psf path in all job settings."""
     for job_list in job_lists:
@@ -405,7 +416,7 @@ def _get_param_df(dct: Mapping[str, Any]) -> pd.DataFrame:
     df = pd.DataFrame(data, columns=columns)
     df.set_index(['key', 'param_type', 'atoms'], inplace=True)
 
-    df['constraints'] = None
+    df['constant'] = False
     df['min'] = -np.inf
     df['max'] = np.inf
     return df
@@ -464,8 +475,12 @@ def _get_prm_frozen(dct: Mapping[str, Union[MutableMapping, Iterable[MutableMapp
     return ret if ret else None
 
 
-def _get_prm_constraints(dct: Mapping[str, Union[MutableMapping, Iterable[MutableMapping]]]
-                         ) -> Generator[Tuple[Optional[dict], float, float], None, None]:
+def _get_prm_constraints(
+    dct: Mapping[str, Union[MutableMapping, Iterable[MutableMapping]]]
+) -> Tuple[
+    Dict[Tuple[str, str], Optional[List[Dict[str, float]]]],
+    List[Tuple[float, float]]
+]:
     """Parse all user-provided constraints.
 
     Yields
@@ -480,6 +495,9 @@ def _get_prm_constraints(dct: Mapping[str, Union[MutableMapping, Iterable[Mutabl
     """
     ignore_keys = {'frozen', 'constraints', 'param', 'unit', 'guess'}
 
+    constraints_dict: Dict[Tuple[str, str], Optional[List[Dict[str, float]]]] = {}
+    min_max: List[Tuple[float, float]] = []
+
     dct_iterator = prm_iter(dct)
     for key_alias, sub_dict in dct_iterator:
         try:
@@ -487,15 +505,54 @@ def _get_prm_constraints(dct: Mapping[str, Union[MutableMapping, Iterable[Mutabl
         except KeyError:
             for k in sub_dict:
                 if k not in ignore_keys:
-                    yield None, -np.inf, np.inf
+                    min_max.append((-np.inf, np.inf))
+            constraints_dict[key_alias, sub_dict["param"]] = []
             continue
 
         extremites, constraints = assign_constraints(proto_constraints)
         for k in sub_dict:
             if k not in ignore_keys:
-                yield (constraints,
-                       extremites.get((k, 'min'), -np.inf),
-                       extremites.get((k, 'max'), np.inf))
+                min_max.append((
+                    extremites.get((k, 'min'), -np.inf),
+                    extremites.get((k, 'max'), np.inf)
+                ))
+        constraints_dict[key_alias, sub_dict["param"]] = (
+            constraints if constraints is not None else []
+        )
+    return constraints_dict, min_max
+
+
+def _parse_ligand_alias(psf_list: Optional[List[PSFContainer]], prm: ParamMapping) -> None:
+    """Replace ``$LIGAND`` constraints with explicit ligands."""
+    not_implemented = psf_list is None or len(psf_list) != 1
+    if not not_implemented:
+        psf: PSFContainer = psf_list[0]
+        lig_id = psf.residue_id.iloc[-1]
+        df = psf.atoms[psf.residue_id == lig_id]
+
+        atom_types: Iterable[str] = df['atom type'].values
+        atom_counter = Counter(atom_types)
+        for k in atom_counter:
+            key = ('charge', 'charge', k)
+            if key not in prm['param'].index:
+                prm['param'].loc[key] = df.loc[df['atom type'] == k, 'charge'].iloc[0]
+                prm['min'][key] = -np.inf
+                prm['max'][key] = np.inf
+                prm['constant'][key] = True
+
+    for lst in prm.constraints.values():
+        for series in lst:
+            if "$LIGAND" not in series:
+                continue
+            elif not_implemented:
+                raise NotImplementedError
+
+            i = series.pop("$LIGAND")
+            for k, v in atom_counter.items():
+                if k in series:
+                    series[k] += v * i
+                else:
+                    series[k] = v * i
 
 
 @overload

FOX/functions/charge_parser.py
@@ -1,0 +1,149 @@
+"""A module with functions related to the parsing of parameter constraints.
+
+Index
+-----
+.. currentmodule:: FOX.functions.charge_parser
+.. autosummary::
+    assign_constraints
+
+API
+---
+.. autofunction:: assign_constraints
+
+"""
+
+from types import MappingProxyType
+from typing import Optional, Dict, Union, Iterable, Tuple,  Generator, overload, List
+
+__all__ = ["assign_constraints"]
+
+ExtremiteDict = Dict[Tuple[str, str], float]
+ConstrainList = List[Dict[str, float]]
+
+#: Map ``"min"`` to ``"max"`` and *vice versa*.
+_INVERT = MappingProxyType({
+    'max': 'min',
+    'min': 'max'
+})
+
+#: Map :math:`>`, :math:`<`, :math:`\ge` and :math:`\le` to either ``"min"`` or ``"max"``.
+_OPPERATOR_MAPPING = MappingProxyType({
+    '<': 'min',
+    '<=': 'min',
+    '>': 'max',
+    '>=': 'max'
+})
+
+
+def assign_constraints(constraints: Union[str, Iterable[str]]
+                       ) -> Tuple[ExtremiteDict, Optional[ConstrainList]]:
+    operator_set = {'>', '<', '*', '==', '+', '-'}
+
+    # Parse integers and floats
+    if isinstance(constraints, str):
+        constraints = [constraints]
+
+    constrain_list = []
+    for item in constraints:
+        for i in operator_set:  # Sanitize all operators; ensure they are surrounded by spaces
+            item = item.replace(i, f'~{i}~')
+
+        item_list = [i.strip().rstrip() for i in item.split('~')]
+        if len(item_list) == 1:
+            continue
+
+        for i, j in enumerate(item_list):  # Convert strings to floats where possible
+            try:
+                float_j = float(j)
+            except ValueError:
+                pass
+            else:
+                item_list[i] = float_j
+
+        constrain_list.append(item_list)
+
+    # Set values in **param**
+    extremite_dict: ExtremiteDict = {}
+    constraints_ = None
+    for constrain in constrain_list:
+        if '==' in constrain:
+            constraints_ = _eq_constraints(constrain)
+        else:
+            extremite_dict.update(_gt_lt_constraints(constrain))
+    return extremite_dict, constraints_
+
+
+def _gt_lt_constraints(constrain: List[str]
+                       ) -> Generator[Tuple[Tuple[str, str], float], None, None]:
+    r"""Parse :math:`>`, :math:`<`, :math:`\ge` and :math:`\le`-type constraints."""
+    for i, j in enumerate(constrain):
+        if j not in _OPPERATOR_MAPPING:
+            continue
+
+        operator, value, atom = _OPPERATOR_MAPPING[j], constrain[i-1], constrain[i+1]
+        if isinstance(atom, float):
+            atom, value = value, atom
+            operator = _INVERT[operator]
+        yield (atom, operator), value
+
+
+@overload
+def _find_float(iterable: Tuple[str]) -> Tuple[str, float]: ...
+@overload
+def _find_float(iterable: Tuple[str, str]) -> Tuple[str, float]: ...
+def _find_float(iterable):  # noqa: E302
+    """Take an iterable of 2 strings and identify which element can be converted into a float."""
+    try:
+        i, j = iterable
+    except ValueError:
+        return iterable[0], 1.0
+
+    if i == "-":
+        return j, -1.0
+    elif j == "-":
+        return i, -1.0
+
+    try:
+        return j, float(i)
+    except ValueError:
+        return i, float(j)
+
+
+def _eq_constraints(constrain_: List[str]) -> ConstrainList:
+    """Parse :math:`a = i * b`-type constraints."""
+    constrain = ''.join(str(i) for i in constrain_).split('==')
+    # import pdb; pdb.set_trace()
+
+    # Assign all other constraints
+    ret = []
+    for _item in constrain:
+        dct: Dict[str, float] = {}
+        if ")" in _item or "(" in _item:
+            raise NotImplementedError(f"Parenthesized constraints are not supported: {_item!r}")
+
+        item_split = _split_operator(_item)
+        for item in item_split:
+            item_list = item.split('*')
+            atom, i = _find_float(item_list)
+            dct[atom] = i
+
+        ret.append(dct)
+    return ret
+
+
+def _split_operator(constrain: str) -> List[str]:
+    plus_split = constrain.split("+")
+    if '-' not in constrain:
+        return plus_split
+
+    ret = []
+    for item in plus_split:
+        if '-' not in item:
+            ret.append(item)
+            continue
+
+        item_split = item.split("-")
+        if item_split[0]:
+            ret.append(item_split[0])
+        ret += [f'-{i}' for i in item_split[1:]]
+    return ret

tests/test_charge_parser.py
@@ -1,0 +1,42 @@
+"""A module for testing :mod:`FOX.functions.charge_parser` ."""
+
+from assertionlib import assertion
+
+from FOX.functions.charge_parser import assign_constraints
+
+
+def test_assign_constraints() -> None:
+    """Test :func:`assign_constraints`."""
+    constraints = [
+        'H < 1',
+        'C >2.0',
+        '3.0> N',
+        '4<O',
+        '1 < F< 2.0',
+        '2 > P >1.0',
+        'S == 2 * Cl == 0.5*Br + -1.5*K - 1*Na == 1* I',
+        '1 < H H < 2',
+    ]
+    extremite, constrain_list = assign_constraints(constraints)
+
+    extremite_ref = {
+        ('H', 'max'): 1.0,
+        ('C', 'min'): 2.0,
+        ('N', 'max'): 3.0,
+        ('O', 'min'): 4.0,
+        ('F', 'min'): 1.0,
+        ('F', 'max'): 2.0,
+        ('P', 'max'): 2.0,
+        ('P', 'min'): 1.0,
+        ('H H', 'min'): 1.0,
+        ('H H', 'max'): 2.0
+    }
+    assertion.eq(extremite, extremite_ref)
+
+    constrain_ref = [
+        {"S": 1.0},
+        {"Cl": 2.0},
+        {"Br": 0.5, "K": -1.5, "Na": -1.0},
+        {"I": 1.0}
+    ]
+    assertion.eq(constrain_list, constrain_ref)

FOX/armc/param_mapping.py
@@ -22,7 +22,7 @@ from logging import Logger
 from functools import wraps, partial
 from typing import (Any, TypeVar, Optional, Hashable, Tuple, Mapping, Iterable, ClassVar, Union,
                     Iterator, KeysView, ItemsView, ValuesView, overload, Callable, FrozenSet, cast,
-                    MutableMapping, TYPE_CHECKING)
+                    MutableMapping, TYPE_CHECKING, Dict)
 
 import numpy as np
 import pandas as pd
@@ -44,17 +44,15 @@ T = TypeVar('T')
 
 # MultiIndex keys
 Tup3 = Tuple[Hashable, Hashable, Hashable]
+Tup2 = Tuple[Hashable, Hashable]
 
 # All dict keys in ParamMappingABC
-SeriesKeys = Literal['min', 'max', 'constraints', 'count']
+SeriesKeys = Literal['min', 'max', 'count', 'constant']
 DFKeys = Literal['param', 'param_old']
-ValidKeys = Literal['param', 'param_old', 'min', 'max', 'constraints', 'count']
+ValidKeys = Union[SeriesKeys, DFKeys]
 
 # A function for moving parameters
 MoveFunc = Callable[[float, float], float]
-
-# A dictionary of constraints
-ConstrainDict = Mapping[Hashable, partial]
 
 # A Mapping representing the conent of ParamMappingABC
 _ParamMappingABC = Mapping[ValidKeys, NDFrame]
@@ -72,8 +70,8 @@ class InputMapping(_InputMapping, total=False):
 
     min: pd.Series
     max: pd.Series
-    constraints: pd.Series
     count: pd.Series
+    constant: pd.Series
 
 
 class Data(TypedDict):
@@ -83,8 +81,8 @@ class Data(TypedDict):
     param_old: pd.DataFrame
     min: pd.Series
     max: pd.Series
-    constraints: pd.Series
     count: pd.Series
+    constant: pd.Series
 
 
 def _parse_param(dct: MutableMapping[str, Any]) -> pd.DataFrame:
@@ -163,8 +161,6 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
         * ``"param_old"`` (:class:`float`): Old paramters from a previous iteration.
         * ``"min"`` (:class:`float`): The minimum value for each parameter.
         * ``"max"`` (:class:`float`): The maximum value for each parameter.
-        * ``"constraints"`` (:class:`object`): A dictionary of constraints for each parameter.
-          Setting a value to ``None`` means that there are no constraints for that parameter.
         * ``"count"`` (:class:`int`): The number of unique atom (pairs) assigned
           to a given parameter.
           Currently only important when updating the charge,
@@ -179,21 +175,23 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
 
     _net_charge: Optional[float]
     _move_range: np.ndarray
-    __data: Data
+    _data_dict: Data
 
     #: Fill values for when optional keys are absent.
     FILL_VALUE: ClassVar[Mapping[ValidKeys, Any]] = MappingProxyType({
         'min': -np.inf,
         'max': np.inf,
-        'constraints': None,  # Dict[str, functools.partial]
-        'count': -1
+        'count': -1,
+        'constant': False,
     })
 
     _PRIVATE_ATTR = frozenset({'_net_charge'})  # type: ignore
 
     def __init__(self, data: Union[InputMapping, pd.DataFrame],
                  move_range: Iterable[float],
-                 func: MoveFunc, **kwargs: Any) -> None:
+                 func: MoveFunc,
+                 constraints: Optional[Mapping[Tup2, Iterable[Mapping[str, float]]]] = None,
+                 **kwargs: Any) -> None:
         r"""Initialize an :class:`ParamMappingABC` instance.
 
         Parameters
@@ -235,8 +233,24 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
         self._data = cast(Data, data)
         self.move_range = cast(np.ndarray, move_range)
         self.func: Callable[[float, float], float] = wraps(func)(partial(func, **kwargs))
+        self.constraints = cast(Dict[Tup2, Tuple[pd.Series, ...]], constraints)
 
     # Properties
+
+    @property
+    def constraints(self) -> Dict[Tup2, Tuple[pd.Series, ...]]:
+        return self._constraints
+
+    @constraints.setter
+    def constraints(self, value: Optional[Mapping[Tup2, Iterable[Mapping[str, float]]]]) -> None:
+        if value is not None:
+            dct = {k: tuple(pd.Series(i) for i in v) for k, v in value.items()}
+        else:
+            dct = {}
+
+        for key in self['param'].index:
+            dct.setdefault(key[:2], ())
+        self._constraints = dct
 
     @property
     def move_range(self) -> np.ndarray:
@@ -267,7 +281,7 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
 
     @property
     def _data(self) -> Data:
-        return getattr(self, '__data')
+        return self._data_dict
 
     @_data.setter
     def _data(self, value: Union[InputMapping, pd.DataFrame]) -> None:
@@ -292,7 +306,7 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
 
         # Construct a dictionary to contain the old parameter
         dct['param_old'] = param.copy()
-        setattr(self, '__data', dct)
+        self._data_dict = cast(Data, dct)
 
     # Magic methods and Mapping implementation
 
@@ -406,8 +420,7 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
             _, prm_type, atoms = idx
             logger.info(f"Moving {prm_type} ({atoms}): {x1:.4f} -> {value:.4f}")
 
-        constraints: ConstrainDict = self['constraints'][idx]
-        ex = self.apply_constraints(idx, value, param_idx, constraints)
+        ex = self.apply_constraints(idx, value, param_idx)
         if ex is not None:
             return ex
 
@@ -450,8 +463,7 @@ class ParamMappingABC(AbstractDataClass, ABC, _ParamMappingABC):
         """  # noqa
         return value
 
-    def apply_constraints(self, idx: Tup3, value: float, param: int,
-                          constraints: ConstrainDict) -> Optional[Exception]:
+    def apply_constraints(self, idx: Tup3, value: float, param: int) -> Optional[Exception]:
         """An optional function for applying further constraints based on **idx** and **value**.
 
         Should perform an inplace update of this instance.
@@ -511,7 +523,8 @@ class ParamMapping(ParamMappingABC):
 
         """  # noqa
         # Define a random parameter
-        random_prm: pd.Series = self['param'][param_idx].sample()
+        variable = ~self['constant']
+        random_prm: pd.Series = self['param'].loc[variable, param_idx].sample()
         idx, x1 = next(random_prm.items())  # Type: Tup3, float
 
         # Define a random move size
@@ -539,8 +552,7 @@ class ParamMapping(ParamMappingABC):
         prm_max: float = self['max'][idx]
         return np.clip(value, prm_min, prm_max)
 
-    def apply_constraints(self, idx: Tup3, value: float, param_idx: int,
-                          constraints: ConstrainDict) -> Optional[ChargeError]:
+    def apply_constraints(self, idx: Tup3, value: float, param_idx: int) -> Optional[ChargeError]:
         """Apply further constraints based on **idx** and **value**.
 
         Performs an inplace update of this instance.
@@ -560,13 +572,16 @@ class ParamMapping(ParamMappingABC):
             A Mapping with the to-be applied constraints per atom (pair).
 
         """  # noqa
-        key, prm_type, _ = idx
-        charge = self._net_charge if prm_type in self.CHARGE_LIKE else None
+        key = idx[:2]
+        atom = idx[2]
+        charge = self._net_charge if key[1] in self.CHARGE_LIKE else None
 
-        constraints_ = None if pd.isnull(constraints) else constraints
-        return update_charge(idx[1:], value, self['param'].loc[key, param_idx],
-                             count=self['count'].loc[key],
-                             prm_min=self['min'].loc[key],
-                             prm_max=self['max'].loc[key],
-                             constrain_dict=constraints_,
-                             net_charge=charge)
+        return update_charge(
+            atom, value,
+            param=self['param'].loc[key, param_idx],
+            count=self['count'].loc[key],
+            atom_coefs=self.constraints[key],
+            prm_min=self['min'].loc[key],
+            prm_max=self['max'].loc[key],
+            net_charge=charge
+        )

commit baac9a9b7e3e507b8159088e1f469030a11225a3

BUG: Fixed three constraint-related bugs (#131)
Fixes 3 issue introduced in https://github.com/nlesc-nano/auto-FOX/pull/124:
* Check if `_sub_prm_dict_frozen` is `None` before iteration.
* Also add constants to `ParamMapping['param_old']`.
* Fixed an issue where negative extremites weren't properly parsed .

FOX/armc/sanitization.py
@@ -234,9 +234,10 @@ def get_param(dct: ParamMapping_) -> Tuple[ParamMapping, dict, dict]:
     constraints, min_max = _get_prm_constraints(_sub_prm_dict)
     data[['min', 'max']] = min_max
 
-    for *_key, value in _get_prm(_sub_prm_dict_frozen):
-        key = tuple(_key)
-        data.loc[key, :] = [value, True, -np.inf, np.inf]
+    if _sub_prm_dict_frozen is not None:
+        for *_key, value in _get_prm(_sub_prm_dict_frozen):
+            key = tuple(_key)
+            data.loc[key, :] = [value, value, True, -np.inf, np.inf]
     data.sort_index(inplace=True)
 
     param_type = prm_dict.pop('type')  # type: ignore
@@ -416,6 +417,7 @@ def _get_param_df(dct: Mapping[str, Any]) -> pd.DataFrame:
     df = pd.DataFrame(data, columns=columns)
     df.set_index(['key', 'param_type', 'atoms'], inplace=True)
 
+    df['param_old'] = df['param'].copy()
     df['constant'] = False
     df['min'] = -np.inf
     df['max'] = np.inf
@@ -536,6 +538,7 @@ def _parse_ligand_alias(psf_list: Optional[List[PSFContainer]], prm: ParamMappin
             key = ('charge', 'charge', k)
             if key not in prm['param'].index:
                 prm['param'].loc[key] = df.loc[df['atom type'] == k, 'charge'].iloc[0]
+                prm['param_old'].loc[key] = prm['param'].loc[key]
                 prm['min'][key] = -np.inf
                 prm['max'][key] = np.inf
                 prm['constant'][key] = True

FOX/functions/charge_parser.py
@@ -76,7 +76,11 @@ def assign_constraints(constraints: Union[str, Iterable[str]]
 def _gt_lt_constraints(constrain: List[str]
                        ) -> Generator[Tuple[Tuple[str, str], float], None, None]:
     r"""Parse :math:`>`, :math:`<`, :math:`\ge` and :math:`\le`-type constraints."""
+    minus = False
     for i, j in enumerate(constrain):
+        if j == '-':
+            minus = True
+            continue
         if j not in _OPPERATOR_MAPPING:
             continue
 
@@ -84,6 +88,9 @@ def _gt_lt_constraints(constrain: List[str]
         if isinstance(atom, float):
             atom, value = value, atom
             operator = _INVERT[operator]
+        if minus:
+            value *= -1
+            minus = False
         yield (atom, operator), value
 
 

tests/test_charge_parser.py
@@ -10,8 +10,8 @@ def test_assign_constraints() -> None:
     constraints = [
         'H < 1',
         'C >2.0',
-        '3.0> N',
-        '4<O',
+        '-3.0> N',
+        '- 4<O',
         '1 < F< 2.0',
         '2 > P >1.0',
         'S == 2 * Cl == 0.5*Br + -1.5*K - 1*Na == 1* I',
@@ -22,8 +22,8 @@ def test_assign_constraints() -> None:
     extremite_ref = {
         ('H', 'max'): 1.0,
         ('C', 'min'): 2.0,
-        ('N', 'max'): 3.0,
-        ('O', 'min'): 4.0,
+        ('N', 'max'): -3.0,
+        ('O', 'min'): -4.0,
         ('F', 'min'): 1.0,
         ('F', 'max'): 2.0,
         ('P', 'max'): 2.0,