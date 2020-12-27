TST: Fix a precision issue in numpy 1.15

tests/test_properties.py
@@ -17,6 +17,13 @@ from qmflows.packages.cp2k_mm import CP2KMM_Result
 from FOX.properties import FromResult, get_pressure, get_bulk_modulus, get_attr
 from FOX.properties.pressure import get_pressure as _get_pressure
 
+# Fix for a precision issue in older numpy versions
+NP_VERSION = tuple(int(i) for i in np.__version__.split('.')[:2])
+if NP_VERSION == (1, 15):
+    RTOL = 1e6
+else:
+    RTOL = 1e7
+
 PATH = Path('tests') / 'test_files'
 
 RESULT = CP2KMM_Result(
@@ -91,15 +98,15 @@ def test_from_result(func: FromResult) -> None:
     """Tests for :class:`FOX.properties.CP2KMM_Result` subclasses."""
     prop1 = func.from_result(RESULT)
     ref1 = np.load(FROM_RESULT_DICT[func])
-    np.testing.assert_allclose(prop1, ref1)
+    np.testing.assert_allclose(prop1, ref1, rtol=RTOL)
 
     prop2 = func.from_result(RESULT, reduce='mean', axis=0)
     ref2 = ref1.mean(axis=0)
-    np.testing.assert_allclose(prop2, ref2)
+    np.testing.assert_allclose(prop2, ref2, rtol=RTOL)
 
     prop3 = func.from_result(RESULT, reduce=np.linalg.norm)
     ref3 = np.linalg.norm(ref1)
-    np.testing.assert_allclose(prop3, ref3)
+    np.testing.assert_allclose(prop3, ref3, rtol=RTOL)
 
 
 @pytest.mark.parametrize('name', GET_ATTR_TUP)