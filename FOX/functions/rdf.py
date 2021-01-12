cca2f82ce50b478a7f4eb5b90a27b5220a19ec9d
Bas van Beek <b.f.van.beek@vu.nl>
2021-01-12 13:25:43 +0100

    REV: Reintroduce the `get_rdf_lowmem` function
    This function is still used in Nano-CAT

--- FOX/functions/rdf.py
+++ FOX/functions/rdf.py
@@ -19,7 +19,7 @@ from typing import Hashable, Iterable
 import numpy as np
 import pandas as pd
 
-__all__ = ['get_rdf_df', 'get_rdf']
+__all__ = ['get_rdf_df', 'get_rdf', 'get_rdf_lowmem']
 
 
 def get_rdf_df(atom_pairs: Iterable[Hashable],
@@ -95,3 +95,50 @@ def get_rdf(dist: np.ndarray,
     dens /= denom
     dens[:, 0] = 0.0
     return dens.sum(axis=0)
+
+
+def get_rdf_lowmem(dist: np.ndarray,
+                   dr: float = 0.05,
+                   r_max: float = 12.0) -> np.ndarray:
+    """Calculate and return the radial distribution function (RDF).
+    The RDF is calculated using the 2D distance matrix **dist**.
+    A more memory efficient implementation of :func:`FOX.functions.rdf.get_rdf`,
+    which operates on a 3D distance matrix.
+    Parameters
+    ----------
+    dist : :math:`n*k` |np.ndarray|_ [|np.float64|_]
+        A 2D array representing a single distance matrices of :math:`n` by :math:`k` atoms.
+    dr : float
+        The integration step-size in Angstrom, *i.e.* the distance between concentric spheres.
+    r_max : float
+        The maximum to be evaluated interatomic distance.
+    Returns
+    -------
+    1D |np.ndarray|_ [|np.float64|_] of length 1 + **r_max** / **dr**:
+        An array with the resulting radial distribution function.
+    """
+    if not dist.size:
+        return np.zeros((), dtype=dist.dtype)
+    elif dist.ndim == 2:
+        dist = dist.reshape((1,) + dist.shape)
+
+    idx_max = 1 + int(r_max / dr)
+    dist_int = np.array(dist / dr, dtype=int).ravel()
+
+    # Calculate the average particle density N / V
+    # The diameter of the spherical volume (V) is defined by the largest inter-particle distance
+    dens_mean = dist.shape[2] / ((4/3) * np.pi * (0.5 * dist.max())**3)
+
+    # Count the number of occurances of each (rounded) distance
+    dens = np.bincount(dist_int, minlength=idx_max)[:idx_max]
+
+    # Correct for the number of reference atoms
+    dens = dens / dist.shape[1]
+    dens[0] = np.nan
+
+    # Convert the particle count into a partical density
+    r = np.arange(0, r_max + dr, dr)
+    dens /= (4 * np.pi * r**2 * dr)
+
+    # Normalize and return the particle density
+    return dens / dens_mean
