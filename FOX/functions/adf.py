cb28fdb81cda34632f182fd760787426499cf2eb
Bas van Beek <b.f.van.beek@vu.nl>
2021-02-26 14:47:14 +0100

    STY: Alias `np.float64` and `np.int64`

--- FOX/functions/adf.py
+++ FOX/functions/adf.py
@@ -34,6 +34,8 @@ from scipy.spatial import cKDTree
 from scipy.spatial.distance import cdist
 
 if TYPE_CHECKING:
+    from numpy import float64 as f8, int64 as i8
+
     _T = TypeVar("_T")
     _SCT = TypeVar("_SCT", bound=np.generic)
 
@@ -44,33 +46,33 @@ __all__ = ['get_adf_df', 'get_adf']
 
 
 def _adf_inner_cdktree(
-    m: NDArray[np.float64],
+    m: NDArray[f8],
     n: int,
     r_max: float,
     idx_list: Iterable[_3Tuple[NDArray[np.integer[Any]]]],
-    boxsize: None | NDArray[np.float64],
-    weight: None | Callable[[NDArray[np.float64]], NDArray[np.float64]] = None,
-) -> List[NDArray[np.float64]]:
+    boxsize: None | NDArray[f8],
+    weight: None | Callable[[NDArray[f8]], NDArray[f8]] = None,
+) -> List[NDArray[f8]]:
     """Perform the loop of :meth:`.init_adf` with a distance cutoff."""
     # Construct slices and a distance matrix
     tree = cKDTree(m, boxsize=boxsize)
-    dist, idx = tree.query(m, n, distance_upper_bound=r_max, p=2)  # type: NDArray[np.float64], NDArray[np.intp]  # noqa: E501
+    dist, idx = tree.query(m, n, distance_upper_bound=r_max, p=2)  # type: NDArray[f8], NDArray[i8]  # noqa: E501
     dist[dist == np.inf] = 0.0
     idx[idx == m.shape[0]] = 0
 
     # Slice the Cartesian coordinates
-    coords13: NDArray[np.float64] = m[idx]
-    coords2: NDArray[np.float64] = m[..., None, :]
+    coords13: NDArray[f8] = m[idx]
+    coords2: NDArray[f8] = m[..., None, :]
 
     # Construct (3D) angle- and distance-matrices
     with np.errstate(divide='ignore', invalid='ignore'):
-        vec: NDArray[np.float64] = ((coords13 - coords2) / dist[..., None])
-        ang: NDArray[np.float64] = np.arccos(np.einsum('jkl,jml->jkm', vec, vec))
+        vec: NDArray[f8] = ((coords13 - coords2) / dist[..., None])
+        ang: NDArray[f8] = np.arccos(np.einsum('jkl,jml->jkm', vec, vec))
         dist = np.maximum(dist[..., None], dist[..., None, :])
     ang[np.isnan(ang)] = 0.0
 
     # Radian (float) to degrees (int)
-    ang_int: NDArray[np.int64] = np.degrees(ang).astype(np.int64)
+    ang_int: NDArray[i8] = np.degrees(ang).astype(np.int64)
 
     # Construct and return the ADF
     ret = []
@@ -82,27 +84,28 @@ def _adf_inner_cdktree(
 
 
 def _adf_inner(
-    m: NDArray[np.float64],
+    m: NDArray[f8],
     idx_list: Iterable[_3Tuple[NDArray[np.integer[Any]]]],
-    weight: None | Callable[[NDArray[np.float64]], NDArray[np.float64]] = None,
-) -> List[NDArray[np.float64]]:
+    lattice: None | NDArray[f8],
+    weight: None | Callable[[NDArray[f8]], NDArray[f8]] = None,
+) -> List[NDArray[f8]]:
     """Perform the loop of :meth:`.init_adf` without a distance cutoff."""
     # Construct a distance matrix
-    dist: NDArray[np.float64] = cdist(m, m)
+    dist: NDArray[f8] = cdist(m, m)
 
     # Slice the Cartesian coordinates
-    coords13: NDArray[np.float64] = m
-    coords2: NDArray[np.float64] = m[..., None, :]
+    coords13: NDArray[f8] = m
+    coords2: NDArray[f8] = m[..., None, :]
 
     # Construct (3D) angle- and distance-matrices
     with np.errstate(divide='ignore', invalid='ignore'):
-        vec: NDArray[np.float64] = ((coords13 - coords2) / dist[..., None])
-        ang: NDArray[np.float64] = np.arccos(np.einsum('jkl,jml->jkm', vec, vec))
+        vec: NDArray[f8] = ((coords13 - coords2) / dist[..., None])
+        ang: NDArray[f8] = np.arccos(np.einsum('jkl,jml->jkm', vec, vec))
         dist = np.maximum(dist[..., None], dist[..., None, :])
     ang[np.isnan(ang)] = 0.0
 
     # Radian (float) to degrees (int)
-    ang_int: NDArray[np.int64] = np.degrees(ang).astype(np.int64)
+    ang_int: NDArray[i8] = np.degrees(ang).astype(np.int64)
 
     # Construct and return the ADF
     ret = []
@@ -137,7 +140,7 @@ def get_adf_df(atom_pairs: Sequence[Hashable]) -> pd.DataFrame:
 def get_adf(
     ang: NDArray[np.integer[Any]],
     weights: None | NDArray[np.number[Any]] = None,
-) -> NDArray[np.float64]:
+) -> NDArray[f8]:
     r"""Calculate and return the angular distribution function (ADF).
 
     Parameters
@@ -159,14 +162,14 @@ def get_adf(
     """
     # Calculate and normalize the density
     denominator = len(ang) / 180
-    at_count: NDArray[np.int64] = np.bincount(ang, minlength=181)[1:181]
-    dens: NDArray[np.float64] = at_count / denominator
+    at_count: NDArray[i8] = np.bincount(ang, minlength=181)[1:181]
+    dens: NDArray[f8] = at_count / denominator
 
     if weights is None:
         return dens
 
     # Weight (and re-normalize) the density based on the distance matrix **dist**
-    area: np.float64 = dens.sum()
+    area: f8 = dens.sum()
     with np.errstate(divide='ignore', invalid='ignore'):
         dens *= np.bincount(ang, weights=weights, minlength=181)[1:181] / at_count
         dens *= area / np.nansum(dens)
