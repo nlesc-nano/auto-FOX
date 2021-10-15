a4b23252b18e32591c311f4fa1e8e959dd29dd98
Bas van Beek <b.f.van.beek@vu.nl>
2021-10-16 01:51:47 +0200

    MAINT,ENH: Rework the auto-FOX properties system

--- FOX/properties/pressure.py
+++ FOX/properties/pressure.py
@@ -1,34 +1,38 @@
 """Functions for calculating the pressure."""
 
-import warnings
-from typing import TypeVar, Callable, Any
+from __future__ import annotations
+
+from typing import Callable, Any, TYPE_CHECKING
 
 import numpy as np
 from scipy import constants
 from scm.plams import Units
 from qmflows.packages.cp2k_package import CP2K_Result
 from qmflows.warnings_qmflows import QMFlows_Warning
+from nanoutils import warning_filter
 
 from . import FromResult
 from ..utils import slice_iter
 
-__all__ = ['get_pressure', 'GetPressure']
+if TYPE_CHECKING:
+    from numpy.typing import NDArray, ArrayLike
+    from numpy import float64 as f8
 
-T = TypeVar("T")
-T1 = TypeVar("T1")
-FT = TypeVar("FT", bound=Callable[..., Any])
+__all__ = ['get_pressure']
 
 
+@FromResult
 def get_pressure(
-    forces,
-    coords,
-    volume,
-    temp=298.15,
-    forces_unit='ha/bohr',
-    coords_unit='bohr',
-    volume_unit='bohr',
-    return_unit='ha/bohr^3',
-):
+    forces: ArrayLike,
+    coords: ArrayLike,
+    volume: ArrayLike,
+    temp: float = 298.15,
+    *,
+    forces_unit: str = 'ha/bohr',
+    coords_unit: str = 'bohr',
+    volume_unit: str = 'bohr',
+    return_unit: str = 'ha/bohr^3',
+) -> NDArray[f8]:
     r"""Calculate the pressure from the passed **forces**.
 
     .. math::
@@ -61,6 +65,9 @@ def get_pressure(
     :class:`np.ndarray[np.float64] <numpy.ndarray>`, shape :math:`(n_{\text{mol}},)`
         A 1D array with all pressures across the trajectory.
 
+
+    .. automethod:: get_pressure.from_result
+
     """  # noqa: E501
     _f = np.asarray(forces, dtype=np.float64) * Units.conversion_ratio(forces_unit, 'ha/bohr')
     xyz = np.asarray(coords, dtype=np.float64) * Units.conversion_ratio(coords_unit, 'bohr')
@@ -82,65 +89,61 @@ def get_pressure(
     return (a + b) * Units.conversion_ratio('ha/bohr^3', return_unit)
 
 
-class GetPressure(FromResult[FT, CP2K_Result]):
-    """A :class:`FOX.properties.FromResult` subclass for :func:`get_pressure`."""
+@get_pressure._set_result_func
+@warning_filter('error', category=QMFlows_Warning)
+def _(
+    self: FromResult[Callable[..., Any]],
+    result: CP2K_Result,
+    *,
+    reduce: None | str | Callable[[Any], Any] = None,
+    axis: None | int | tuple[int, ...] = None,
+    return_unit: str = 'ha/bohr^3',
+    **kwargs: Any,
+) -> Any:
+    r"""Call :func:`get_pressure` using argument extracted from **result**.
 
-    @property
-    def __call__(self):  # noqa: D205, D400
-        """
-        Note
-        ----
-        Using :meth:`get_pressure.from_result() <FromResult.from_result>` requires the passed
-        :class:`qmflows.CP2K_Result <qmflows.packages.cp2k_package.CP2K_Result>`
-        to have access to the following files for each argument:
+    Parameters
+    ----------
+    result : :class:`qmflows.CP2K_Result <qmflows.packages.cp2k_package.CP2K_Result>`
+        The Result instance that **self** should operator on.
+    reduce : :class:`str` or :class:`Callable[[Any], Any] <collections.abc.Callable>`, optional
+        A callback for reducing the output of **self**.
+        Alternativelly, one can provide on of the string aliases
+        from :attr:`~FromResult.REDUCTION_NAMES`.
+    axis : :class:`int` or :class:`Sequence[int] <collections.abc.Sequence>`, optional
+        The axis along which the reduction should take place.
+        If :data:`None`, use all axes.
+    return_unit : :class:`str`
+        The unit of the to-be returned quantity.
+    \**kwargs : :data:`~typing.Any`
+        Further keyword arguments for :func:`get_pressure`.
 
-        * **forces**: ``cp2k-frc-1.xyz``
-        * **coords**: ``cp2k-pos-1.xyz``
-        * **volume**: ``cp2k-1.cell``
-        * **temp**: ``cp2k-1.ener``
+    Returns
+    -------
+    :data:`~typing.Any`
+        The output of :func:`get_pressure`.
 
-        """
-        return self._func
+    """  # noqa: E501
+    if result.status in {'failed', 'crashed'}:
+        raise RuntimeError(f"Cannot extract data from a job with status {result.status!r}")
 
-    def from_result(self, result, reduce=None, axis=None, *, return_unit='ha/bohr^3', **kwargs):
-        r"""Call **self** using argument extracted from **result**.
+    a_to_au = Units.conversion_ratio('angstrom', 'bohr')
+    forces = self._pop(
+        kwargs, 'forces',
+        callback=lambda: getattr(result, 'forces'),
+    )
+    temp = self._pop(
+        kwargs, 'temp',
+        callback=lambda: getattr(result, 'temperature'),
+    )
+    coords = self._pop(
+        kwargs, 'coords',
+        callback=lambda: getattr(result, 'coordinates') * a_to_au,
+    )
+    volume = self._pop(
+        kwargs, 'volume',
+        callback=lambda: getattr(result, 'volume') * a_to_au**3,
+    )
 
-        Parameters
-        ----------
-        result : :class:`qmflows.CP2K_Result <qmflows.packages.cp2k_package.CP2K_Result>`
-            The Result instance that **self** should operator on.
-        reduce : :class:`str` or :class:`Callable[[Any], Any] <collections.abc.Callable>`, optional
-            A callback for reducing the output of **self**.
-            Alternativelly, one can provide on of the string aliases from :attr:`REDUCTION_NAMES`.
-        axis : :class:`int` or :class:`Sequence[int] <collections.abc.Sequence>`, optional
-            The axis along which the reduction should take place.
-            If :data:`None`, use all axes.
-        return_unit : :class:`str`
-            The unit of the to-be returned quantity.
-        \**kwargs : :data:`~typing.Any`
-            Further keyword arguments for :meth:`__call__`.
-
-        Returns
-        -------
-        :data:`~typing.Any`
-            The output of :meth:`__call__`.
-
-        """  # noqa: E501
-        if result.status in {'failed', 'crashed'}:
-            raise RuntimeError(f"Cannot extract data from a job with status {result.status!r}")
-
-        a_to_au = Units.conversion_ratio('angstrom', 'bohr')
-        with warnings.catch_warnings():
-            warnings.simplefilter('error', QMFlows_Warning)
-
-            forces = self._pop(kwargs, 'forces', callback=lambda: getattr(result, 'forces'))
-            temp = self._pop(kwargs, 'temp', callback=lambda: getattr(result, 'temperature'))
-            coords = self._pop(
-                kwargs, 'coords', callback=lambda: getattr(result, 'coordinates') * a_to_au
-            )
-            volume = self._pop(
-                kwargs, 'volume', callback=lambda: getattr(result, 'volume') * a_to_au**3
-            )
-
-        ret = self(forces, coords, volume, temp, return_unit=return_unit, **kwargs)
-        return self._reduce(ret, reduce, axis)
+    ret = self(forces, coords, volume, temp, return_unit=return_unit, **kwargs)
+    return self._reduce(ret, reduce, axis)
