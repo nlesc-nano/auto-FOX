e3a55aa7c3f6ee00f58013551073272999553d51
Bas van Beek <b.f.van.beek@vu.nl>
2021-11-17 13:56:23 +0100

    BLD: Update dependencies

--- setup.py
+++ setup.py
@@ -23,11 +23,10 @@ docs_require = [
 tests_require_no_optional = [
     'pytest>=5.4.0',
     'pytest-cov',
-    'auto-FOX-data@git+https://github.com/nlesc-nano/auto-FOX-data@1.1.8',
 ]
 tests_require = [
     'ase>=3.21.1',
-    'CAT@git+https://github.com/nlesc-nano/CAT@master',
+    'nlesc-CAT>=0.10.0',
 ]
 tests_require += tests_require_no_optional
 tests_require += docs_require
@@ -109,7 +108,7 @@ setup(
         'AssertionLib>=2.3',
         'noodles>=0.3.3',
         'h5py>=2.10',
-        'qmflows@git+https://github.com/SCM-NV/qmflows@master',
+        'qmflows>=0.11.0',
         'plams>=1.5.1',
     ],
     setup_requires=[
